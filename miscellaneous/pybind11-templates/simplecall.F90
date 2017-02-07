5da71044587e4e43298291bfedb9d11a8c566580
Simon <s.lopez@brgm.fr>
2017-02-07 15:39:25 +0100

    Simple fortran/C++ function call from python through pybind11.
    This was tested against Pybind11 2.0.1 release
    https://github.com/pybind/pybind11/releases

---
+++ miscellaneous/pybind11-templates/simplecall.F90
@@ -0,0 +1,34 @@
+
+    module example_module
+
+    use, intrinsic :: iso_c_binding
+
+    implicit none
+
+    contains
+
+    
+    function my_fortran_add(a, b) result(S) bind(C)
+
+    integer(c_int), value, intent(in)   :: a, b
+    integer(c_int)                      :: S
+
+    write(*, *) 'Hello from Fortran!'
+    S = a + b
+    write(*,*) 'The Fortran sum: ', a, ' + ', b, ' evaluates to ', S
+
+    end function my_fortran_add
+
+    
+    function my_fortran_add_byref(a, b) result(S) bind(C)
+
+    integer(c_int), intent(in)          :: a, b
+    integer(c_int)                      :: S
+
+    write(*, *) 'Hello from Fortran!'
+    S = a + b
+    write(*,*) 'The Fortran sum: ', a, ' + ', b, ' evaluates to ', S
+
+    end function my_fortran_add_byref
+
+    end module example_module
