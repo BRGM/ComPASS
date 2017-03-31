bb9fd07a674ddef99e85fd65954e00eff11f1a5d
Simon <s.lopez@brgm.fr>
2017-03-31 14:42:55 +0200

    Test making directories through python.

---
+++ miscellaneous/pybind11-templates/wrap_make_directory.cpp
@@ -0,0 +1,9 @@
+#include <Python.h>
+
+int main(int argc, char *argv[])
+{
+	Py_Initialize();
+	PyRun_SimpleString("from os import makedirs\n"
+		               "makedirs('foodir/foosubdir')\n");
+	Py_Finalize();
+}
