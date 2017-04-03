a49cddb67e012fd627b9767b7423b4e464b9103d
Simon <s.lopez@brgm.fr>
2017-04-03 02:15:58 +0200

    Fix VTK interferences with pybind11

---
+++ src/wrappers/GlobalMeshWrapper.F90
@@ -0,0 +1,29 @@
+
+    module GlobalMeshWrapper
+
+      use, intrinsic :: iso_c_binding
+
+      use GlobalMesh
+
+      implicit none
+
+      type, bind(C) :: cpp_vertices
+        type(c_ptr)       :: p
+        integer(c_size_t) :: n
+      end type cpp_vertices
+
+      public :: &
+        vertices_buffer
+
+    contains
+
+      subroutine vertices_buffer(buffer) bind(C, name="vertices_buffer")
+
+        type(cpp_vertices), intent(inout) :: buffer
+        
+        buffer%p = c_loc( XNode(1,1) )
+        buffer%n = size(XNode, 1)
+
+      end subroutine vertices_buffer
+
+    end module GlobalMeshWrapper
