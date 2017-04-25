8f488938c8fe47fea8ef6b2d4416101589ed91e0
Simon <s.lopez@brgm.fr>
2017-04-25 19:07:40 +0200

    BugFix Still directory creation.
    This is a safer and (slighty) lighter way.

--- src/wrappers/PathUtilities.cpp
+++ src/wrappers/PathUtilities.cpp
@@ -10,14 +10,16 @@ extern "C"
   //          Otherwise encapsulation between Py_Initialize() / Py_Finalize() is needed.
   void c_make_directory(const char * s)
   {
+    //std::cerr << "%% creating directory: *|" << s << "|*" << std::endl;
     std::string python_code;
     python_code.append("from os import makedirs\n");
-    python_code.append("if not os.path.exists('").append(s).append("'):\n");
-	python_code.append("    try:\n");
-	python_code.append("        makedirs('").append(s).append("')\n");
-	python_code.append("    except FileExistsError:\n");
-	// CHECKME: Silently ignores if target directory already exists this may be necessary due to concurrent calls to the routine
-	python_code.append("        pass\n");
-	PyRun_SimpleString(python_code.c_str());
+    //python_code.append("print('%% python attempt at: ").append(s).append("')\n");
+    python_code.append("try:\n");
+    python_code.append("  makedirs('").append(s).append("')\n");
+    //python_code.append("  print('%% python created: ").append(s).append("')\n");
+    python_code.append("except FileExistsError:\n");
+    // CHECKME: Silently ignores if target directory already exists this may be necessary due to concurrent calls to the routine
+    python_code.append("  pass\n");
+    PyRun_SimpleString(python_code.c_str());
   }
 }
