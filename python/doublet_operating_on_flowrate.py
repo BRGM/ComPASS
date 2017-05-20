23fbff65b2c56855f10941454b5ec9cf2db0401c
Simon <s.lopez@brgm.fr>
2017-05-20 14:51:11 +0200

    WIP Bug
    With well operating on flow rate simulation crashes with the following trace:
    ============= error stack trace ====================
    [1] ERROR: zero diagonal in local row 1369
           iluk_mpi_bj  file= ilu_mpi_bj.c  line= 154
    
    [1] called from: factor_private  file= Euclid_dh.c  line= 512
    [1] called from: Euclid_dhSetup  file= Euclid_dh.c  line= 248
    [1] called from: HYPRE_EuclidSetup  file= HYPRE_parcsr_Euclid.c  line= 277

---
+++ python/doublet_operating_on_flowrate.py
@@ -0,0 +1,52 @@
+import ComPASS
+import doublet_utils
+from ComPASS.utils.units import *
+
+ComPASS.set_output_directory_and_logfile(__file__)
+
+grid = ComPASS.Grid(
+    shape = (31, 21, 1),
+    extent = (3000., 2000., 20),
+    origin = (-1500, -1000, -1600),
+)
+
+def make_wells():
+	#Ox, Oy = center(grid)[:2]
+	producer = doublet_utils.make_well((-500, 0))
+	producer.operate_on_flowrate = 1 * ton / hour, 1E5
+	producer.produce()
+	injector = doublet_utils.make_well((500, 0))
+	injector.operate_on_flowrate = 1 * ton / hour, 30E6
+	injector.inject(degC2K(30))
+	return [producer, injector]
+
+ComPASS.init(
+    grid = grid,
+    wells = make_wells
+)
+
+@ComPASS.on_master_proc
+def print_iteration_info():
+    print()
+    print('Time Step (iteration):', n)
+    print('Current time: %.1f years' % (ComPASS.get_current_time() / year), ' -> final time:', final_time / year)
+    print('Timestep: %.3f days' % (ComPASS.get_timestep() / day))
+
+final_time = 30 * year
+n = 0
+output_frequency = 1 * year
+t_output = 0
+while ComPASS.get_current_time() <= final_time:
+    n+= 1
+    print_iteration_info()
+    ComPASS.make_timestep()
+    t = ComPASS.get_current_time()
+    if t > t_output:
+        ComPASS.output_visualization_files(n)
+    # WARNING / CHECKME we may loose some outputs
+    while (t_output < t):
+        t_output = t_output + output_frequency
+    ComPASS.timestep_summary()
+
+ComPASS.finalize()
+
