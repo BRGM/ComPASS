d9bc1bfd8977d4123bbee0e8c6bd64b4f3a59e79
Simon <s.lopez@brgm.fr>
2017-07-21 17:20:05 +0200

    Acces timestep control from python

--- src/wrappers/GlobalVariables_wrappers.F90
+++ src/wrappers/GlobalVariables_wrappers.F90
@@ -13,39 +13,67 @@
           get_delta_t, &
           get_final_time, &
           set_final_time, &
-          get_gravity
+          get_gravity, &
+          get_initial_timestep, &
+          get_maximum_timestep, &
+          set_initial_timestep, &
+          set_maximum_timestep
 
     contains
 
        function get_current_time() result(t) &
           bind(C, name="get_current_time")
-       real(c_double) :: t
-       t = TimeCurrent
+          real(c_double) :: t
+          t = TimeCurrent
        end function get_current_time
 
        function get_delta_t() result(t) &
           bind(C, name="get_delta_t")
-       real(c_double) :: t
-       t = Delta_t
+          real(c_double) :: t
+          t = Delta_t
        end function get_delta_t
 
        function get_final_time() result(t) &
           bind(C, name="get_final_time")
-       real(c_double) :: t
-       t = TimeFinal
+          real(c_double) :: t
+          t = TimeFinal
        end function get_final_time
 
        subroutine set_final_time(t) &
           bind(C, name="set_final_time")
-       real(c_double), value, intent(in) :: t
-       TimeFinal = t
+          real(c_double), value, intent(in) :: t
+          TimeFinal = t
        end subroutine set_final_time
 
-        function get_gravity() result(g) &
+       function get_gravity() result(g) &
           bind(C, name="gravity")
-        real(c_double) :: g
-        g = Gravite
-        end function get_gravity
+          real(c_double) :: g
+          g = Gravite
+       end function get_gravity
 
-end module GlobalVariablesWrappers
+       function get_initial_timestep() result(t) &
+          bind(C, name="get_initial_timestep")
+          real(c_double) :: t
+          t = TimeStepInit
+       end function get_initial_timestep
+
+       function get_maximum_timestep() result(t) &
+          bind(C, name="get_maximum_timestep")
+          real(c_double) :: t
+          t = TimeStepMax
+       end function get_maximum_timestep
+
+       subroutine set_initial_timestep(t) &
+          bind(C, name="set_initial_timestep")
+          real(c_double), value, intent(in) :: t
+          TimeStepInit = t
+       end subroutine set_initial_timestep
+
+       subroutine set_maximum_timestep(t) &
+          bind(C, name="set_maximum_timestep")
+          real(c_double), value, intent(in) :: t
+          TimeStepMax = t
+       end subroutine set_maximum_timestep
+
+    end module GlobalVariablesWrappers
 
--- src/wrappers/GlobalVariables_wrappers.cpp
+++ src/wrappers/GlobalVariables_wrappers.cpp
@@ -4,8 +4,12 @@ extern "C"
 	double get_current_time();
 	double get_delta_t();
 	double get_final_time();
+	double get_initial_timestep();
+	double get_maximum_timestep();
 	double gravity();
 	void set_final_time(double);
+	void set_initial_timestep(double);
+	void set_maximum_timestep(double);
 }
 
 #include "GlobalVariables_wrappers.h"
@@ -17,6 +21,10 @@ void add_global_variables_wrappers(py::module& module)
 	module.def("get_timestep", &get_delta_t);
 	module.def("get_final_time", &get_final_time);
 	module.def("set_final_time", &set_final_time);
+	module.def("get_initial_timestep", &get_initial_timestep);
+	module.def("set_initial_timestep", &set_initial_timestep);
+	module.def("get_maximum_timestep", &get_maximum_timestep);
+	module.def("set_maximum_timestep", &set_maximum_timestep);
 	module.def("gravity", &gravity);
 
 }
