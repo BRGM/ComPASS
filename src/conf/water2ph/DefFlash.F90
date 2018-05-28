d7af566cd46e5e0436ae0575d9c3da413edcd66f
Simon Lopez <s.lopez@brgm.fr>
2018-05-28 22:05:50 +0200

    Factorize flash of control volumes

--- src/conf/water2ph/DefFlash.F90
+++ src/conf/water2ph/DefFlash.F90
@@ -16,44 +16,13 @@ module DefFlash
    use Thermodynamics
    use IncCVReservoir
    use VAGFrac ! for rocktypes
-   use DefFlashWells
 
    implicit none
 
    public :: &
-      DefFlash_Flash ! Flash after each Newton iteration
-
-   private :: &
       DefFlash_Flash_cv
 
 contains
-
-   !> \brief Main surboutine, after each Newton iteration
-   !! execute the flash to determine the phases
-   !! which are actualy present, and
-   !! the mode of the well (flowrate or pressure).
-   subroutine DefFlash_Flash
-
-      integer :: k
-      double precision :: Psat, dTsat, Tsat
-
-      do k = 1, NbNodeLocal_Ncpus(commRank + 1)
-         call DefFlash_Flash_cv(IncNode(k), NodeRocktypeLocal(:, k), PoroVolDarcyNode(k))
-      end do
-
-      do k = 1, NbFracLocal_Ncpus(commRank + 1)
-         call DefFlash_Flash_cv(IncFrac(k), FracRocktypeLocal(:, k), PoroVolDarcyFrac(k))
-      end do
-
-      do k = 1, NbCellLocal_Ncpus(commRank + 1)
-         call DefFlash_Flash_cv(IncCell(k), CellRocktypeLocal(:, k), PoroVolDarcyCell(k))
-      end do
-
-      ! choose between linear or non-linear update of the Newton unknown Pw
-      ! The next subroutines also compute the mode of the wells ('pressure' or 'flowrate')
-      call DefFlashWells_NewtonFlashLinWells
-
-   end subroutine DefFlash_Flash
 
    !> \brief Determine the phases
    !! which are actualy present.
