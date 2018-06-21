856f4f687274442a46835d90f0454cc14c09c23f
Simon Lopez <s.lopez@brgm.fr>
2018-06-21 15:00:22 +0200

    Update test case

--- test/bulk/test-more_faults_than_nodes.py
+++ test/bulk/test-more_faults_than_nodes.py
@@ -11,8 +11,8 @@ import numpy as np
 import ComPASS
 from ComPASS.utils.units import *
 from ComPASS.timeloops import standard_loop
-import ComPASS.GridTools as GT
-MT = ComPASS.ComPASS.MeshTools
+import MeshTools as MT
+import MeshTools.GridTools as GT
 #import vtkwriters as vtkw
 
 L = 1E3
@@ -22,7 +22,7 @@ gridshape = (n,) * 3
 vertices, tets = GT.grid2tets(gridshape, gridextent)
 tets = np.asarray(tets, dtype=MT.idtype())
 
-mesh = MT.tetmesh(vertices, tets)
+mesh = MT.TetMesh.make(vertices, tets)
 
 # The plane x-y=0
 def on_plane_xy(pts, epsilon):
@@ -36,26 +36,37 @@ def on_plane_yz(pts, epsilon):
 def on_plane(pts, epsilon=1E-10*L):
     return on_plane_xy(pts, epsilon) | on_plane_xz(pts, epsilon) | on_plane_yz(pts, epsilon)
 
-zmax = mesh.vertices[:, -1].max()
-topnodes = np.nonzero(mesh.vertices[:, -1]==zmax)[0]
-
+ComPASS.load_eos('water2ph')
 ComPASS.set_output_directory_and_logfile(__file__)
+print('Gravity:', ComPASS.get_gravity())
+
+def toponodes():
+    vertices = ComPASS.global_vertices()
+    z = vertices[:, -1]
+    return z==z.max()
 
 def select_fractures():
-    where = np.zeros(mesh.nb_faces(), dtype=np.bool)
+    where = np.zeros(mesh.nb_faces, dtype=np.bool)
     centers = ComPASS.compute_global_face_centers()
     where = on_plane(centers)
-    print('Nb of nodes', mesh.nb_vertices(), 'vs', np.count_nonzero(where), 'fracture faces')
+    print('Nb of nodes', mesh.nb_vertices, 'vs', np.count_nonzero(where), 'fracture faces')
     return where
 
 def select_dirichlet_nodes():
-    print('Selecting', topnodes.shape[0], 'top nodes.')
-    on_top = np.zeros(mesh.nb_vertices(), dtype=np.bool)
-    on_top[topnodes] = True
+    vertices = ComPASS.global_vertices()
+    z = vertices[:, -1]
+    on_top = toponodes()
+    print('Selecting', np.sum(on_top), 'top nodes.')
     return on_top
+
+def set_global_flags():
+    nodeflags = ComPASS.global_nodeflags()
+    nodeflags[:] = 0
+    nodeflags[toponodes()] = 1
 
 def set_boundary_conditions():
     dirichlet = ComPASS.dirichlet_node_states()
+    topnodes = ComPASS.nodeflags() == 1 
     dirichlet.p[topnodes] = 1E5
     dirichlet.T[topnodes] = degC2K(30)
     dirichlet.context[:] = 2
@@ -70,12 +81,17 @@ def set_initial_values():
         state.S[:] = [0, 1]
         state.C[:] = 1.
 
-print('Gravity:', ComPASS.gravity())
-
 ComPASS.init(
     mesh = mesh,
     fracture_faces = select_fractures,
-    set_dirichlet_nodes = select_dirichlet_nodes
+    set_dirichlet_nodes = select_dirichlet_nodes,
+    cell_permeability = 1e-15,
+    cell_porosity = 0.1,
+    cell_thermal_conductivity = 2,
+    fracture_permeability = 1e-15,
+    fracture_porosity = 0.5,
+    fracture_thermal_conductivity = 2,
+    set_global_flags = set_global_flags,
 )
 set_boundary_conditions()
 set_initial_values()
