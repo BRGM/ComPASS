36093f7ba682752bffa7c6c16a21465db433a663
Amira <Amira.El_Hani@Bordeaux-inp.fr>
2018-08-28 14:19:28 +0200

    correction de la solution exacte en 2D

--- test/bulk/2D_gradient_flow_analytical.py
+++ test/bulk/2D_gradient_flow_analytical.py
@@ -2,61 +2,57 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 import math
+from math import *
 from scipy.special import erfc
 
 
-#calcul de la solution exacte
 Phi = 0.2
 rhow = 1000
-
 b = 5e+03
 rhoCp = 1000*2000
-Cm = 60+273   #température pour x=0
-gamma = 0
-mu = 0
-U = 1/(Phi+rhoCp*(1-Phi)/(rhow*b))*b #*densité molaire à priori constante
-Dx = 1
 Rd = rhow*Phi*b+rhoCp*(1-Phi)
-P = (U**2 + 4*Dx*Rd*(mu+gamma))**(1/2)
+mu = b*rhow/Rd
+C0 = 60+273   #température pour x=0
+gamma = 0 #pas sur
+mu = 1
+Lx, Ly, Lz = 100., 50., 1.
+n0 = Ly
+epsilon = 8
+n = 100
+#U = 1/(Phi+rhoCp*(1-Phi)/(rhow*b))*b #*densité molaire à priori constante
+#Dx = 1
+#P = (U**2 + 4*Dx*Rd*(mu+gamma))**(1/2)
 
-def exact_sol(x,t):
-    x = np.asarray(x)
-    _where = x < 200
-    exact_sol = np.zeros(x.shape, dtype=np.double)
-    xw = x[_where]
-    type(xw)
+def exact_sol(x,y,t):
+    #exact_sol = np.zeros(x.shape, dtype=np.double)
     print(Dx,Rd,t)
-    erfc((Rd*xw-P*t)/(2*(Dx*Rd*t)**0.5))
-    type(exact_sol[_where])
-    exact_sol[xw,t] = 1/2*Cm*np.exp(gamma*t)*(np.exp(xw*(U-P)/(2*Dx))*erfc((Rd*xw-P*t)/(2*(Dx*Rd*t)**0.5))+np.exp(xw*(U+P)/(2*Dx))*erfc((Rd*xw+P*t)/(2*(Dx*Rd*t)**0.5)))
-    #where = np.logical_not(_where)
-    #xw = x[_where]
+    exact_sol(x,y,t) = epsilon*C0/(2*n0)*(np.erfc((x-mu*t)/sqrt(4*Dl*t))+np.exp(mu*x/Dl)*np.erfc((x+mu*t)/sqrt(4*Dl*t))
+    for i in range(n)
+        l1 = C0/(i*np.pi)*np.sin(i*np.pi*epsilon/n0)*np.cos(i*np.pi*yw/n0)
+        l2 = np.exp(x/2*(mu/Dl-sqrt(mu/Dl+(2*i*np.pi/n0)**2*Dt/Dl))
+        l3 = np.erfc((x-Dl*t*((mu/Dl)**2+(2*i*np.pi/n0)**2*Dt/Dl)/sqrt(4*Dl*t)))
+        l4 = np.exp(x/2*(mu/Dl+sqrt((mu/Dl)**2+(2*i*np.pi/n0)**2*Dt/Dl)))
+        l5 = np.erfc((x+Dl*t*sqrt((mu/Dl)**2+(2*i*np.pi/n0)*Dt/Dl))/sqrt(4*Dl*t))
+        exact_sol(x,y,t) = exact_sol(x,y,t) + l1-l2-l3+l4-l5
     return exact_sol
 
-Lx, Ly, Lz = 100., 10., 1.
-dx = 2
+t = 100
+dt = 2
+nt = math.floor(t/dt)
 
-day = 2. # seconds choix!!!!! relation avec np.arrange()
-x = np.arange(0, Lx , dx)
-t = day * np.hstack([0.1, np.arange(20, 1020, 30), 1E8/day])
-table = np.array([exact_sol(ti, x) for ti in t>0])
+def test():
+    nx = 100
+    ny = 50
+    x=np.linspace(0,Lx,nx)
+    y=np.linspace(-Ly/2,Ly/2,ny)
+    xx, yy = np.meshgrid(x, y)
 
-csv_table = np.hstack([np.reshape(t/day, (-1, 1)),np.reshape(t, (-1, 1)),table])
-
-np.savetxt('2D_GF_ANALYTICAL.csv', csv_table,
-        delimiter=';',
-        header='seconds;' + ';'.join('%.1f'%xi for xi in x)
-)
-
-try:
-    import matplotlib.pyplot as plt
-except ImportError:
-    print('No figure is drawn as matpplotlib is not installed.')
-    raise
-else:
-    plt.clf()
-    plt.plot(x, table[-1])
-    plt.xlabel('distance (m)')
-    plt.ylabel('temperature (deg C)')
-    plt.grid(True)
-    plt.savefig('2D_GF_ANALYTICAL.png')
+    for k in range(nt):
+        c=exact_sol(xx,yy,t)
+        if ((k+1)%10==0):
+            plt.subplot(211)
+            cs = plt.contourf(x,y,c2)
+            fig.colorbar(cs)
+            plt.draw()
+            plt.pause(0.1)
+            plt.clf()
