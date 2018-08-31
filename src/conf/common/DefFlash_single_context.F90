44bb69d4b380c2f3f3f97f629e4329b62ee8037f
Simon Lopez <s.lopez@brgm.fr>
2018-08-31 16:57:38 +0200

    Tidying up

---
+++ src/conf/common/DefFlash_single_context.F90
@@ -0,0 +1,40 @@
+!
+! This file is part of ComPASS.
+!
+! ComPASS is free software: you can redistribute it and/or modify it under both the terms
+! of the GNU General Public License version 3 (https://www.gnu.org/licenses/gpl.html),
+! and the CeCILL License Agreement version 2.1 (http://www.cecill.info/licences/Licence_CeCILL_V2.1-en.html).
+!
+    
+! No flash, typically when a single phase is present
+    
+module DefFlash
+
+   use IncCVReservoir
+
+   implicit none
+
+   integer, parameter :: SINGLE_CONTEXT = 1
+   
+   public :: &
+      DefFlash_Flash_cv
+
+contains
+
+   !> \brief Determine the phases
+   !! which are actualy present.
+   !!
+   !! Applied to IncNode, IncFrac and IncCell.
+   !! \param[in]      porovol   porous Volume ?????
+   !! \param[inout]   inc       Unknown (IncNode, IncFrac or IncCell)
+   subroutine DefFlash_Flash_cv(inc, rocktype, porovol)
+
+      type(TYPE_IncCVReservoir), intent(inout) :: inc
+      integer, intent(in) :: rocktype(IndThermique + 1)
+      double precision, intent(in) :: porovol ! porovol
+
+      inc%ic = SINGLE_CONTEXT
+
+   end subroutine DefFlash_Flash_cv
+
+end module DefFlash
--- src/conf/linear_water/DefFlash.F90
+++ src/conf/linear_water/DefFlash.F90
@@ -6,39 +6,4 @@
 ! and the CeCILL License Agreement version 2.1 (http://www.cecill.info/licences/Licence_CeCILL_V2.1-en.html).
 !
 
-! Model: 2 phase 2 comp, context switch
-
-!> \brief Define the flash to determine the phases
-!! which are actualy present in each cell, and
-!! the mode of the well (flowrate or pressure).
-module DefFlash
-
-   use Thermodynamics
-   use IncCVReservoir
-   use VAGFrac ! for rocktypes
-   use DefFlashWells
-
-   implicit none
-
-   public :: &
-      DefFlash_Flash_cv
-
-contains
-
-   !> \brief Determine the phases
-   !! which are actualy present.
-   !!
-   !! Applied to IncNode, IncFrac and IncCell.
-   !! \param[in]      porovol   porous Volume ?????
-   !! \param[inout]   inc       Unknown (IncNode, IncFrac or IncCell)
-   subroutine DefFlash_Flash_cv(inc, rocktype, porovol)
-
-      type(TYPE_IncCVReservoir), intent(inout) :: inc
-      INTEGER, INTENT(IN) :: rocktype(IndThermique + 1)
-      double precision, intent(in) :: porovol ! porovol
-
-      inc%ic = 1
-
-   end subroutine DefFlash_Flash_cv
-
-end module DefFlash
+include '../common/DefFlash_single_context.F90'
