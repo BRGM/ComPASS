60e24c57cb99daaecf2e30c86c54a73a16e7493e
Amira <Amira.El_Hani@Bordeaux-inp.fr>
2018-09-05 11:06:13 +0200

    sauvegarde de la solution finale

--- test/bulk/2D_gradient_flow_analytical.py
+++ test/bulk/2D_gradient_flow_analytical.py
@@ -1,12 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
 import ComPASS
 import time
 import math
 from math import *
 from scipy import special
-
-
 
 #Dx = 1
 #P = (U**2 + 4*Dx*Rd*(mu+gamma))**(1/2)
@@ -22,10 +21,8 @@ def exact_sol(x,y,t):
         exact_sol = exact_sol +l1*(l2*l3+l4*l5)
     return exact_sol
 
-
-
 def test():
-    nx = 100
+    nx = 500
     ny = 50
     nz = 1
     x=np.linspace(0,Lx,nx)
@@ -35,7 +32,7 @@ def test():
         t = (k+1)*dt
         print(k)
         c=exact_sol(xx,yy,t)
-        if ((k+1)%1==0):
+        if ((k+1)%10==0):
             fig = plt.figure(1)
             plt.subplot(211)
             cs = plt.contourf(x,y,c)
@@ -43,14 +40,18 @@ def test():
             plt.title('t='+str(t))
             plt.draw()
             plt.pause(1)
+            #plt.savefig('exact_sol.png')
+            pp = PdfPages('exact_sol.pdf')
+            pp.savefig()
             #plt.savefig(('exact_sol_'+str(t)),format='png')
             plt.clf()
 
 
 if __name__ == '__main__':
-    year = 3.15e+7
-    tfinal = 2e+6
-    dt = 0.2 * tfinal
+    year = 5
+    tfinal = 100
+
+    dt = 1
     nt = math.floor(tfinal/dt)
     Phi = 0.2
     rhow = 1000
@@ -58,15 +59,15 @@ if __name__ == '__main__':
     rhoCp = 1000*2000
     Rd = rhow*Phi*b+rhoCp*(1-Phi)
     print(Rd)
-    U = 3e-5
+    #U = 1/(Phi+rhoCp*(1-Phi)/(rhow*b))*b*rhow #*densité molaire à priori constante
+    U = 5e-3
     mu = b*rhow/Rd*U
+    #mu = 1
     print(mu)
     C0 = 60+273  #température en x=0
     gamma = 0 #pas sur dans l'expression de P
-    #mu = 1
-    Dl = 2/Rd  #conductivité thermique/Rd
-    Dt = 2/Rd
-    #U = 1/(Phi+rhoCp*(1-Phi)/(rhow*b))*b*rhow #*densité molaire à priori constante
+    Dl = 2e+8/Rd  #conductivité thermique/Rd
+    Dt = 2e+8/Rd
     print(Dt)
     Lx, Ly, Lz = 100., 50., 1.
     n0 = Ly
