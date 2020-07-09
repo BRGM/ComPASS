BRGM/ComPASS

commit 636d806f13b84c7da5091b8302f9cc6db480ad63

Add well node parent index wrt well nodes
Previous parent indexing becomes parent offset and is relative to all wells CSR indexing.

ComPASS/dump_wells.py
@@ -78,6 +78,7 @@ def _print_well_stats(simulation, well_type):
         )
         print("vertices:", well.vertices)
         print("parent (vertex id):", well.parent_vertex)
+        print("parent (csr offset):", well.parent_offset)
         print("parent (rank):", well.parent_rank)
         print("pressure:", well.pressure)
         print("temperature:", K2degC(well.temperature))
@@ -167,7 +168,7 @@ def _dump_wells(simulation, well_type, outputdir=None, tag=None, verbose=False):
                 np.hstack(
                     [
                         np.reshape(remap[well_vertices[:-1]], (-1, 1)),
-                        np.reshape(remap[well.parent_vertex], (-1, 1)),
+                        np.reshape(remap[well.parent_offset], (-1, 1)),
                     ]
                 ),
                 pointdata=welldata,

.gitlab-ci.yml
@@ -95,6 +95,7 @@ sanity_check:
     - docker run --rm -v $PWD/test/unit:/localfs $CI_REGISTRY_IMAGE:$CI_COMMIT_REF_SLUG run postprocess_producers.py
     - docker run --rm -v $PWD/test/unit:/localfs $CI_REGISTRY_IMAGE:$CI_COMMIT_REF_SLUG run check_petrophysics.py
     - docker run --rm -v $PWD/test/unit:/localfs $CI_REGISTRY_IMAGE:$CI_COMMIT_REF_SLUG run dof_families.py
+    - docker run --rm -v $PWD/test/unit:/localfs $CI_REGISTRY_IMAGE:$CI_COMMIT_REF_SLUG run well_connectivity.py
 
 pytest_run:
   stage: test
@@ -141,6 +142,7 @@ parallel_test:
     - docker run --rm -v $PWD/test/unit:/localfs $CI_REGISTRY_IMAGE:$CI_COMMIT_REF_SLUG run --parallel check_petrophysics.py
     - docker run --rm -v $PWD/test/unit:/localfs $CI_REGISTRY_IMAGE:$CI_COMMIT_REF_SLUG run --parallel dof_families.py
     - docker run --rm -v $PWD/test/bulk:/localfs $CI_REGISTRY_IMAGE:$CI_COMMIT_REF_SLUG run --parallel chain_random_wells.py
+    - docker run --rm -v $PWD/test/unit:/localfs $CI_REGISTRY_IMAGE:$CI_COMMIT_REF_SLUG run --parallel well_connectivity.py
 
 time_bulk_scripts:
   stage: test

src/core/GlobalMesh.F90
@@ -1161,6 +1161,7 @@ contains
       NodebyWell%Num = -1
       NodeDatabyWell%Num = -1
       NodeDatabyWell%Val(:)%PtParent = -1
+      NodeDatabyWell%Val(:)%RelParent = -1
 
       NodebyWell%Pt(1) = 0
       do i = 1, NbWell

src/core/LocalMesh.F90
@@ -2003,6 +2003,7 @@ contains
    !> \brief Contains the local data for the wells (own+ghost) of proc ip
    !! in particular %Parent contains the local number of the node Parent                   <br>
    !!               %Parent remains -1 if head node of the well
+! FIXME: Refactor with the same function to treat successively injectors and producers
    subroutine LocalMesh_NodeDatabyWellLocal(ip)
 
       integer, intent(in) :: ip
@@ -2019,6 +2020,8 @@ contains
       allocate (NodeDatabyWellInjLocal_Ncpus(ip1)%Num(nnz))
       allocate (NodeDatabyWellInjLocal_Ncpus(ip1)%Val(nnz))
 
+      ! copy CSR offsets
+      ! FIXME: would be shared using (shared... ?) pointers...
       NodeDatabyWellInjLocal_Ncpus(ip1)%Pt(:) = NodebyWellInjLocal_Ncpus(ip1)%Pt(:)
       NodeDatabyWellInjLocal_Ncpus(ip1)%Num(:) = NodebyWellInjLocal_Ncpus(ip1)%Num(:)
 
@@ -2039,13 +2042,24 @@ contains
             endif
          enddo
 
+#ifndef NDEBUG
+         if (NodeDatabyWellInjLocal_Ncpus(ip1)%Val(NodebyWellInjLocal_Ncpus(ip1)%Pt(k + 1))%Parent /= -1) &
+            call CommonMPI_abort("Inconsistent injector head")
+#endif
          ! Find pointer of parent and fill ...%Val%PtParent (except for head node)
          do cpt = NodebyWellInjLocal_Ncpus(ip1)%Pt(k) + 1, NodebyWellInjLocal_Ncpus(ip1)%Pt(k + 1) - 1
+            ! reservoir node index
             num_parent = NodeDatabyWellInjLocal_Ncpus(ip1)%Val(cpt)%Parent
+#ifndef NDEBUG
+            if (num_parent == -1) &
+               call CommonMPI_abort("Inconsistent parent vertex in injector")
+#endif
             ! look for parent (stored after son)
             do Data_cpt = cpt + 1, NodebyWellInjLocal_Ncpus(ip1)%Pt(k + 1)
                if (NodeDatabyWellInjLocal_Ncpus(ip1)%Num(Data_cpt) == num_parent) then
                   NodeDatabyWellInjLocal_Ncpus(ip1)%Val(cpt)%PtParent = Data_cpt
+                  NodeDatabyWellInjLocal_Ncpus(ip1)%Val(cpt)%RelParent = &
+                     Data_cpt - NodebyWellInjLocal_Ncpus(ip1)%Pt(k)
                   exit
                endif
             enddo
@@ -2062,6 +2076,8 @@ contains
       allocate (NodeDatabyWellProdLocal_Ncpus(ip1)%Num(nnz))
       allocate (NodeDatabyWellProdLocal_Ncpus(ip1)%Val(nnz))
 
+      ! copy CSR offsets
+      ! FIXME: would be shared using (shared... ?) pointers...
       NodeDatabyWellProdLocal_Ncpus(ip1)%Pt(:) = NodebyWellProdLocal_Ncpus(ip1)%Pt(:)
       NodeDatabyWellProdLocal_Ncpus(ip1)%Num(:) = NodebyWellProdLocal_Ncpus(ip1)%Num(:)
 
@@ -2082,13 +2098,25 @@ contains
             endif
          enddo
 
+#ifndef NDEBUG
+         if (NodeDatabyWellProdLocal_Ncpus(ip1)%Val(NodebyWellProdLocal_Ncpus(ip1)%Pt(k + 1))%Parent /= -1) &
+            call CommonMPI_abort("Inconsistent producer head")
+#endif
+
          ! Find pointer of parent and fill ...%Val%PtParent (except for head node)
          do cpt = NodebyWellProdLocal_Ncpus(ip1)%Pt(k) + 1, NodebyWellProdLocal_Ncpus(ip1)%Pt(k + 1) - 1
+            ! reservoir node index
             num_parent = NodeDatabyWellProdLocal_Ncpus(ip1)%Val(cpt)%Parent
+#ifndef NDEBUG
+            if (num_parent == -1) &
+               call CommonMPI_abort("Inconsistent parent vertex in producer")
+#endif
             ! look for parent (stored after son)
             do Data_cpt = cpt + 1, NodebyWellProdLocal_Ncpus(ip1)%Pt(k + 1)
                if (NodeDatabyWellProdLocal_Ncpus(ip1)%Num(Data_cpt) == num_parent) then
                   NodeDatabyWellProdLocal_Ncpus(ip1)%Val(cpt)%PtParent = Data_cpt
+                  NodeDatabyWellProdLocal_Ncpus(ip1)%Val(cpt)%RelParent = &
+                     Data_cpt - NodebyWellProdLocal_Ncpus(ip1)%Pt(k)
                   exit
                endif
             enddo

src/core/MeshSchema.F90
@@ -729,8 +729,8 @@ contains
       integer stat(MPI_STATUS_SIZE)
 
       integer :: blen(1), offsets(1), oldtypes(1), MPI_IDNODE
-      integer :: blocklen(4), arraytype(4)
-      integer(kind=MPI_ADDRESS_KIND) ::disp(4)
+      integer :: blocklen(5), arraytype(5)
+      integer(kind=MPI_ADDRESS_KIND) ::disp(5)
 
       integer :: MPI_WELLDATA_ID
 
@@ -1586,21 +1586,17 @@ contains
 #endif
 
       ! MPI TYPE for DataNodewell: MPI_DATANODEWELL
-      blocklen(1) = 1
-      blocklen(2) = 1
-      blocklen(3) = 1
-      blocklen(4) = 1
-      arraytype(1) = MPI_INTEGER
-      arraytype(2) = MPI_INTEGER
-      arraytype(3) = MPI_DOUBLE
-      arraytype(4) = MPI_DOUBLE
+      blocklen = 1
+      arraytype(1:3) = MPI_INTEGER
+      arraytype(4:5) = MPI_DOUBLE
       disp(1) = 0 !  = 0
       disp(2) = 4 !  + integer
       disp(3) = 8 !  + integer
-      disp(4) = 16 ! + double
+      disp(4) = 12 !  + integer
+      disp(5) = 20 ! + double
 
       ! Create and commit
-      call MPI_Type_Create_Struct(4, blocklen, disp, arraytype, MPI_DATANODEWELL, Ierr)
+      call MPI_Type_Create_Struct(5, blocklen, disp, arraytype, MPI_DATANODEWELL, Ierr)
       call MPI_Type_commit(MPI_DATANODEWELL, Ierr)
 
       ! send NodeDatabyWell

src/wells/DefWell.F90
@@ -51,8 +51,9 @@ module DefWell
 
    !> Store data of one Node Well about parent and well index
    type, bind(C) :: TYPE_DataNodeWell
-      integer(c_int) :: Parent !< num of parent; -1 if head node
-      integer(c_int) :: PtParent !< pt of parent; -1 if head node ! FIXME: improve doc !!!
+      integer(c_int) :: Parent !< num of parent = reservoir node; -1 if head node
+      integer(c_int) :: PtParent !< pt of parent = offset in "CSR" - Fortran indexing; -1 if head node ! FIXME: improve doc !!!
+      integer(c_int) :: RelParent !< index of parent locally to each well - Fortran indexing; -1 if head node ! FIXME: improve doc !!!
       real(c_double) :: WID !< Well Index Darcy
       real(c_double) :: WIF !< Well Index Fourier
    end type TYPE_DataNodeWell
@@ -489,6 +490,7 @@ contains
 
       x2%Parent = x1%Parent
       x2%PtParent = x1%PtParent
+      x2%RelParent = x1%RelParent
       x2%WID = x1%WID
       x2%WIF = x1%WIF
 

src/wrappers/Well_wrappers.cpp
@@ -54,7 +54,8 @@ struct Perforation_state  // WellPerforationState_type in IncCVWell.F90
 struct Perforation_data  // Fortran TYPE_DataNodeWell
 {
    int parent_vertex_id;  // num of parent; -1 if head node
-   int parent_rank;  // pt of parent; -1 if head node ! FIXME: improve doc !!!
+   int parent_offset;  // pt of parent; -1 if head node ! FIXME: improve doc !!!
+   int parent_rank;    // pt of parent; -1 if head node ! FIXME: improve doc !!!
    double well_index_Darcy;
    double well_index_Fourier;
 };
@@ -510,6 +511,7 @@ void add_well_wrappers(py::module& module) {
 
    py::class_<Perforation_data>(module, "PerforationData")
        .def_readonly("parent_vertex", &Perforation_data::parent_vertex_id)
+       .def_readonly("parent_offset", &Perforation_data::parent_offset)
        .def_readonly("parent_rank", &Perforation_data::parent_rank)
        .def_readonly("well_index_Darcy", &Perforation_data::well_index_Darcy)
        .def_readonly("well_index_Fourier",
@@ -629,8 +631,13 @@ void add_well_wrappers(py::module& module) {
       });
    };
 
+   // parent reservoir node
    add_perforation_rank_property("parent_vertex",
                                  offsetof(Perforation_data, parent_vertex_id));
+   // parent offset in *wells CSR*
+   add_perforation_rank_property("parent_offset",
+                                 offsetof(Perforation_data, parent_offset));
+   // parent local node index
    add_perforation_rank_property("parent_rank",
                                  offsetof(Perforation_data, parent_rank));
 

test/unit/well_connectivity.py
@@ -1,0 +1,61 @@
+#
+# This file is part of ComPASS.
+#
+# ComPASS is free software: you can redistribute it and/or modify it under both the terms
+# of the GNU General Public License version 3 (https://www.gnu.org/licenses/gpl.html),
+# and the CeCILL License Agreement version 2.1 (http://www.cecill.info/licences/Licence_CeCILL_V2.1-en.html).
+#
+
+import numpy as np
+
+import ComPASS
+import ComPASS.mpi as mpi
+
+simulation = ComPASS.load_eos("water2ph")
+ComPASS.set_output_directory_and_logfile(__file__)
+
+shape = nx, ny, nz = 4, 4, 6
+grid = ComPASS.Grid(shape=shape, extent=shape, origin=(0, 0, 0),)
+
+
+def make_wells():
+    wells = []
+    for x in [1, 2]:
+        well = simulation.create_vertical_well((x, 1), 0.1)
+        well.operate_on_flowrate = 1.0, -np.inf
+        well.produce()
+        wells.append(well)
+        well = simulation.create_vertical_well((x, 2), 0.1)
+        well.operate_on_flowrate = 1.0, np.inf
+        well.inject(293.13)
+        wells.append(well)
+    return wells
+
+
+simulation.init(
+    mesh=grid,
+    wells=make_wells,
+    cell_porosity=0.1,
+    cell_permeability=1.0,
+    cell_thermal_conductivity=1.0,
+)
+
+for well_type, info in [
+    ("Producer", simulation.producers_information()),
+    ("Injector", simulation.injectors_information()),
+]:
+    for k, well in enumerate(info):
+        print(f"Proc {mpi.proc_rank}: {well_type} {k} well vertices:", well.vertices)
+        print(
+            f"Proc {mpi.proc_rank}: {well_type} {k} parent reservoir node:",
+            well.parent_vertex,
+        )
+        print(
+            f"Proc {mpi.proc_rank}: {well_type} {k} parent CSR offset:",
+            well.parent_offset,
+        )
+        print(
+            f"Proc {mpi.proc_rank}: {well_type} {k} parent well node:", well.parent_rank
+        )
+        # must always be true
+        assert np.all(well.vertices[well.parent_rank] == well.parent_vertex)

commit 96e13257f4f2c59ce9227a964ae4b12a6af1a88a

BugFix Wrong parent index in well output
Bug was introduced by commit 636d806f13b84c7da5091b8302f9cc6db480ad63.

ComPASS/dump_wells.py
@@ -168,7 +168,7 @@ def _dump_wells(simulation, well_type, outputdir=None, tag=None, verbose=False):
                 np.hstack(
                     [
                         np.reshape(remap[well_vertices[:-1]], (-1, 1)),
-                        np.reshape(remap[well.parent_offset], (-1, 1)),
+                        np.reshape(remap[well.parent_vertex], (-1, 1)),
                     ]
                 ),
                 pointdata=welldata,