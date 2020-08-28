969b05369ce2ea2be0f47918e879026667718dee
Simon Lopez <s.lopez@brgm.fr>
2020-08-28 13:31:13 +0200

    Two examples to document bug #280

---
+++ test/bugs/280/newton_memleak.py
@@ -0,0 +1,42 @@
+# This file is part of ComPASS.
+#
+# ComPASS is free software: you can redistribute it and/or modify it under both the terms
+# of the GNU General Public License version 3 (https://www.gnu.org/licenses/gpl.html),
+# and the CeCILL License Agreement version 2.1 (http://www.cecill.info/licences/Licence_CeCILL_V2.1-en.html).
+#
+
+import sys
+import numpy as np
+
+import ComPASS
+from ComPASS.utils.units import *
+from ComPASS.timeloops import standard_loop
+from ComPASS.simulation_context import SimulationContext
+from ComPASS.newton import Newton, default_Newton
+from ComPASS.linalg.factory import linear_solver
+from ComPASS.utils.memory import MemStatus
+
+# column permeability in m^2 (low permeability -> bigger time steps)
+k_matrix = 1e-12
+# column porosity
+phi_matrix = 0.15
+# bulk thermal conductivity in W/m/K
+K_matrix = 2.0
+
+ComPASS.set_output_directory_and_logfile(__file__)
+simulation = ComPASS.load_eos("water2ph")
+
+grid = ComPASS.Grid(shape=(10, 10, 10), extent=(2, 1, 1), origin=(0, 0, 0),)
+
+simulation.init(
+    mesh=grid,
+    cell_permeability=k_matrix,
+    cell_porosity=phi_matrix,
+    cell_thermal_conductivity=K_matrix,
+)
+
+mem = MemStatus("loop", skip_first=True)
+for _ in range(200):
+    newton = default_Newton(simulation)
+    mem.update(verbose=True)
+print(mem)
---
+++ test/bugs/280/two_cells_memleak.py
@@ -0,0 +1,88 @@
+# This file is part of ComPASS.
+#
+# ComPASS is free software: you can redistribute it and/or modify it under both the terms
+# of the GNU General Public License version 3 (https://www.gnu.org/licenses/gpl.html),
+# and the CeCILL License Agreement version 2.1 (http://www.cecill.info/licences/Licence_CeCILL_V2.1-en.html).
+#
+
+import sys
+import numpy as np
+
+import ComPASS
+from ComPASS.utils.units import *
+from ComPASS.timeloops import standard_loop
+from ComPASS.simulation_context import SimulationContext
+from ComPASS.newton import Newton, default_Newton
+from ComPASS.linalg.factory import linear_solver
+from ComPASS.utils.memory import MemStatus
+
+# initial reservoir pressure
+pL, pR = 2.0 * bar, 1.0 * bar
+# initial reservoir temperature - convert Celsius degrees to Kelvin degrees
+TL, TR = (
+    degC2K(30),
+    degC2K(20),
+)
+# column permeability in m^2 (low permeability -> bigger time steps)
+k_matrix = 1e-12
+# column porosity
+phi_matrix = 0.15
+# bulk thermal conductivity in W/m/K
+K_matrix = 2.0
+
+simulation = ComPASS.load_eos("water2ph")
+ComPASS.set_output_directory_and_logfile(__file__)
+
+grid = ComPASS.Grid(shape=(2, 1, 1), extent=(2, 1, 1), origin=(0, 0, 0),)
+
+
+def dirichlet_nodes():
+    x = simulation.global_vertices()[:, 0]
+    return (x == x.min()) & (x == x.max())
+
+
+simulation.init(
+    mesh=grid,
+    cell_permeability=k_matrix,
+    cell_porosity=phi_matrix,
+    cell_thermal_conductivity=K_matrix,
+    set_dirichlet_nodes=dirichlet_nodes,
+)
+
+initial_state = simulation.build_state(simulation.Context.liquid, p=pR, T=TR)
+simulation.all_states().set(initial_state)
+dirichlet = simulation.dirichlet_node_states()
+dirichlet.set(initial_state)
+x = simulation.vertices()[:, 0]
+left = x == x.min()
+dirichlet.p[left] = pL
+dirichlet.T[left] = TL
+
+# lsolver = linear_solver(simulation, direct=True)
+# newton = Newton(simulation, 1e-5, 8, lsolver)
+# newton = default_Newton(simulation)
+
+t0 = 0
+nitermax = 0
+mem = MemStatus("script", skip_first=False)
+
+import os, psutil
+from ComPASS.utils.memory import sizeof_fmt
+
+process = psutil.Process(os.getpid())
+mem0 = process.memory_info().rss
+print(f"Starting memory:", sizeof_fmt(mem0))
+
+for _ in range(2000):
+    nitermax += 2
+    t0 = simulation.standard_loop(
+        initial_time=t0,
+        initial_timestep=1,
+        nitermax=nitermax,
+        output_period=1,  # , newton=newton,
+    )
+    mem.update(verbose=True)
+
+print(mem)
+print(f"Final memory:", sizeof_fmt(process.memory_info().rss))
+print(f"Final memory increase:", sizeof_fmt(process.memory_info().rss - mem0))
