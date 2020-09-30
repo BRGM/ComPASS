d8c32b970b0359bb28c151783b1747a97d948b5f
Simon Lopez <s.lopez@brgm.fr>
2020-09-30 13:57:36 +0200

    Workaround Reference vs. copy problem
    well_data_provider returns a copy instead of a reference to the actual data.

--- ComPASS/wells/connections.py
+++ ComPASS/wells/connections.py
@@ -1,8 +1,8 @@
 from collections import namedtuple, defaultdict
 from itertools import chain
+from functools import partial
 
 from .. import mpi
-from .wells import get_well_data
 
 
 WellHead = namedtuple(
@@ -13,10 +13,11 @@ WellHead = namedtuple(
 
 
 class WellDataConnection:
-    def __init__(self, wid, source, mpi_tag=None, dest=None, data=None):
+    def __init__(self, wid, source, mpi_tag=None, dest=None, data=None, provider=None):
+        assert data is None or provider is None
         self.well_id = wid
         self.source = source
-        self.well_data = data
+        self._well_data = provider or (lambda: data)
         # print(
         #     f"New connection {(wid, source, mpi_tag, dest)} on proc {mpi.proc_rank}"
         #     f"{'' if self.well_data is None else ' with data'}"
@@ -28,6 +29,10 @@ class WellDataConnection:
             self.dest = [proc for proc in dest if proc != mpi.proc_rank]
         self.mpi_tag = mpi_tag
         self._value = WellHead()
+
+    @property
+    def well_data(self):
+        return self._well_data()
 
     @property
     def value(self):
@@ -60,12 +65,14 @@ class WellDataConnections:
     def __init__(self):
         self.wells = {}
 
-    def add(self, wid, source, mpi_tag=None, dest=None, data=None):
+    def add(self, wid, source, mpi_tag=None, dest=None, data=None, provider=None):
         assert wid not in self.wells
         assert mpi_tag is None or mpi_tag not in [
             val.mpi_tag for val in self.wells.values()
         ]
-        self.wells[wid] = WellDataConnection(wid, source, mpi_tag, dest, data)
+        self.wells[wid] = WellDataConnection(
+            wid, source, mpi_tag, dest, data=data, provider=provider
+        )
 
     def synchronize(self):
         # WARNING: the two loops must NOT be merged
@@ -161,11 +168,18 @@ def add_well_connections(
     connections, well_data_provider, well_pairs=None, proc_requests=None
 ):
     """
+    :param well_data_provider: a function that will consume a well id and return some compliant data
     :param well_pairs: a sequence of well pair to be chained
     :param proc_requests: a sequence of pair (proc, list of wells to make available)
     """
+    print("**********", connections)
     new_connections = _define_connections(
         well_data_provider, well_pairs=well_pairs, proc_requests=proc_requests
     )
     for connection in new_connections:
-        connections.add(*connection, data=well_data_provider(connection[0]))
+        # FIXME: the following does not work if well_data_provider returns a copy
+        #        instead of a reference to the actual data
+        # connections.add(*connection, data=well_data_provider(connection[0]))
+        connections.add(
+            *connection, provider=partial(well_data_provider, connection[0])
+        )
