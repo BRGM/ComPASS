64870fd4d16a84593640e04b27ced9f6d118aee0
Simon Lopez <s.lopez@brgm.fr>
2020-10-07 20:40:44 +0200

    Tolerance for grid boundary utilities

--- ComPASS/utils/grid.py
+++ ComPASS/utils/grid.py
@@ -2,6 +2,14 @@ def grid_center(grid):
     return tuple(grid.origin[i] + 0.5 * grid.extent[i] for i in range(3))
 
 
+def below(axis, lim):
+    return lambda pts: pts[:, axis] <= lim
+
+
+def above(axis, lim):
+    return lambda pts: pts[:, axis] >= lim
+
+
 def on_xvalue(x):
     return lambda pts: pts[:, 0] == x
 
@@ -14,106 +22,114 @@ def on_zvalue(z):
     return lambda pts: pts[:, 2] == z
 
 
-def on_xmin(grid):
-    return on_xvalue(grid.origin[0])
+def below_axis_limit(grid, axis, epsilon=0):
+    return below(axis, grid.origin[axis] + epsilon)
 
 
-def on_xmax(grid):
-    return on_xvalue(grid.origin[0] + grid.extent[0])
+def above_axis_limit(grid, axis, epsilon=0):
+    return above(axis, grid.origin[axis] + grid.extent[axis] - epsilon)
 
 
-def on_ymin(grid):
-    return on_yvalue(grid.origin[1])
+def on_xmin(grid, epsilon=0):
+    return below_axis_limit(grid, 0, epsilon)
 
 
-def on_ymax(grid):
-    return on_yvalue(grid.origin[1] + grid.extent[1])
+def on_xmax(grid, epsilon=0):
+    return above_axis_limit(grid, 0, epsilon)
 
 
-def on_zmin(grid):
-    return on_zvalue(grid.origin[2])
+def on_ymin(grid, epsilon=0):
+    return below_axis_limit(grid, 1, epsilon)
 
 
-def on_zmax(grid):
-    return on_zvalue(grid.origin[2] + grid.extent[2])
+def on_ymax(grid, epsilon=0):
+    return above_axis_limit(grid, 1, epsilon)
 
 
-def on_vertical_boundaries(grid):
+def on_zmin(grid, epsilon=0):
+    return below_axis_limit(grid, 2, epsilon)
+
+
+def on_zmax(grid, epsilon=0):
+    return above_axis_limit(grid, 2, epsilon)
+
+
+def on_vertical_boundaries(grid, epsilon=0):
     return (
-        lambda pts: on_xmin(grid)(pts)
-        | on_xmax(grid)(pts)
-        | on_ymin(grid)(pts)
-        | on_ymax(grid)(pts)
+        lambda pts: on_xmin(grid, epsilon)(pts)
+        | on_xmax(grid, epsilon)(pts)
+        | on_ymin(grid, epsilon)(pts)
+        | on_ymax(grid, epsilon)(pts)
     )
 
 
-def on_any_boundary(grid):
+def on_any_boundary(grid, epsilon=0):
     return (
-        lambda pts: on_vertical_boundaries(grid)(pts)
-        | on_zmin(grid)(pts)
-        | on_zmax(grid)(pts)
+        lambda pts: on_vertical_boundaries(grid, epsilon)(pts)
+        | on_zmin(grid)(pts, epsilon)
+        | on_zmax(grid)(pts, epsilon)
     )
 
 
-def vertical_boundaries(simulation, grid):
+def vertical_boundaries(simulation, grid, epsilon=0):
     def select():
         vertices = simulation.global_vertices()
-        return on_vertical_boundaries(grid)(vertices)
+        return on_vertical_boundaries(grid, epsilon)(vertices)
 
     return select
 
 
-def xmin_boundary(simulation, grid):
+def xmin_boundary(simulation, grid, epsilon=0):
     def select():
         vertices = simulation.global_vertices()
-        return on_xmin(grid)(vertices)
+        return on_xmin(grid, epsilon)(vertices)
 
     return select
 
 
-def xmax_boundary(simulation, grid):
+def xmax_boundary(simulation, grid, epsilon=0):
     def select():
         vertices = simulation.global_vertices()
-        return on_xmax(grid)(vertices)
+        return on_xmax(grid, epsilon)(vertices)
 
     return select
 
 
-def ymin_boundary(simulation, grid):
+def ymin_boundary(simulation, grid, epsilon=0):
     def select():
         vertices = simulation.global_vertices()
-        return on_ymin(grid)(vertices)
+        return on_ymin(grid, epsilon)(vertices)
 
     return select
 
 
-def ymax_boundary(simulation, grid):
+def ymax_boundary(simulation, grid, epsilon=0):
     def select():
         vertices = simulation.global_vertices()
-        return on_ymax(grid)(vertices)
+        return on_ymax(grid, epsilon)(vertices)
 
     return select
 
 
-def bottom_boundary(simulation, grid):
+def bottom_boundary(simulation, grid, epsilon=0):
     def select():
         vertices = simulation.global_vertices()
-        return on_zmin(grid)(vertices)
+        return on_zmin(grid, epsilon)(vertices)
 
     return select
 
 
-def top_boundary(simulation, grid):
+def top_boundary(simulation, grid, epsilon=0):
     def select():
         vertices = simulation.global_vertices()
-        return on_zmax(grid)(vertices)
+        return on_zmax(grid, epsilon)(vertices)
 
     return select
 
 
-def all_boundaries(simulation, grid):
+def all_boundaries(simulation, grid, epsilon=0):
     def select():
         vertices = simulation.global_vertices()
-        return on_any_boundary(grid)(vertices)
+        return on_any_boundary(grid, epsilon)(vertices)
 
     return select
