8eecbdab04f222339ba85b587e9a7ca2bd52e134
Simon Lopez <s.lopez@brgm.fr>
2021-01-11 16:08:59 +0100

    Doublet in gaz reservoir

---
+++ test/bulk/doublet_on_cartesian_grid_gas.py
@@ -0,0 +1,69 @@
+#
+# This file is part of ComPASS.
+#
+# ComPASS is free software: you can redistribute it and/or modify it under both the terms
+# of the GNU General Public License version 3 (https://www.gnu.org/licenses/gpl.html),
+# and the CeCILL License Agreement version 2.1 (http://www.cecill.info/licences/Licence_CeCILL_V2.1-en.html).
+#
+
+import ComPASS
+from ComPASS.utils.units import *
+from ComPASS.utils.grid import grid_center
+
+# fmt: off
+pres = 50. * MPa            # initial reservoir pressure
+Tres = degC2K( 350. )        # initial reservoir temperature - convert Celsius degrees to Kelvin degrees
+Tinjection = degC2K( 110. )  # injection temperature - convert Celsius to Kelvin degrees
+Qm = 300. * ton / hour      # production flowrate
+interwell_distance = 1 * km # distance between wells
+omega_reservoir = 0.15      # reservoir porosity
+k_reservoir = 1E-12         # reservoir permeability in m^2
+K_reservoir = 2             # bulk thermal conductivity in W/m/K
+# fmt: on
+
+Lx, Ly, Lz = 3000.0, 2000.0, 100.0
+Ox, Oy, Oz = -1500.0, -1000.0, -1600.0
+nx, ny, nz = 30, 20, 10
+
+simulation = ComPASS.load_eos("water2ph")
+ComPASS.set_output_directory_and_logfile(__file__)
+simulation.set_gravity(0)
+
+grid = ComPASS.Grid(shape=(nx, ny, nz), extent=(Lx, Ly, Lz), origin=(Ox, Oy, Oz),)
+
+
+def make_wells():
+    Cx, Cy, Cz = grid_center(grid)
+    producer = simulation.create_vertical_well((Cx - 0.5 * interwell_distance, Cy))
+    producer.operate_on_flowrate = Qm, 1.0 * bar
+    producer.produce()
+    injector = simulation.create_vertical_well((Cx + 0.5 * interwell_distance, Cy))
+    injector.operate_on_flowrate = -Qm, pres + 100.0 * MPa
+    injector.inject(Tinjection)
+    return (producer, injector)
+
+
+simulation.init(
+    mesh=grid,
+    set_dirichlet_nodes=simulation.vertical_boundaries(grid),
+    wells=make_wells,
+    cell_porosity=omega_reservoir,
+    cell_permeability=k_reservoir,
+    cell_thermal_conductivity=K_reservoir,
+)
+
+X0 = simulation.build_state(simulation.Context.gas, p=pres, T=Tres)
+simulation.all_states().set(X0)
+simulation.dirichlet_node_states().set(X0)
+
+simulation.standard_loop(
+    initial_timestep=1 * hour, final_time=30 * year, output_period=year,
+)
+
+# You can reload any simulation state from an output directory
+# Here we will use the current output directory
+snapshot_directory = simulation.runtime.output_directory
+# The mesh and its partition must be exactly the same
+t = simulation.reload_snapshot(snapshot_directory, 28)
+# From here you could use a new simulation.standard_loop
+# to continue simulation from the reloaded state
