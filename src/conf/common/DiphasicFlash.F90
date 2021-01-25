11463ab7dfb43464aa0db8f447d76c9089066cf2
Simon Lopez <s.lopez@brgm.fr>
2021-01-25 22:24:36 +0100

    Helpers to initialize diphasic states

--- src/conf/common/DiphasicFlash.F90
+++ src/conf/common/DiphasicFlash.F90
@@ -1,19 +1,4 @@
 
-    !< enforce C in [0,1] and sum equal to 1
-    pure subroutine DiphasicFlash_enforce_consistent_molar_fractions(inc)
-       type(Type_IncCVReservoir), intent(inout) :: inc
-
-       integer :: alpha
-       real(c_double) :: Ca
-
-       do alpha = 1, NbPhase ! NbPhase = 2
-          Ca = min(max(inc%Comp(AIR_COMP, alpha), 0.d0), 1.d0)
-          inc%Comp(AIR_COMP, alpha) = Ca
-          inc%Comp(WATER_COMP, alpha) = 1.d0 - Ca
-       enddo
-
-    end subroutine DiphasicFlash_enforce_consistent_molar_fractions
-
     pure subroutine DiphasicFlash_liquid_to_diphasic(inc, pa, dpadS)
        type(Type_IncCVReservoir), intent(inout) :: inc
        real(c_double), intent(in) :: pa(NbPhase) ! p^\alpha: phase pressure
@@ -105,3 +90,19 @@
        endif
 
     end subroutine DiphasicFlash_Flash_cv
+
+    !< enforce C in [0,1] and sum equal to 1
+    pure subroutine DiphasicFlash_enforce_consistent_molar_fractions(inc) &
+       bind(C, name="DiphasicFlash_enforce_consistent_molar_fractions")
+       type(Type_IncCVReservoir), intent(inout) :: inc
+
+       integer :: alpha
+       real(c_double) :: Ca
+
+       do alpha = 1, NbPhase ! NbPhase = 2
+          Ca = min(max(inc%Comp(AIR_COMP, alpha), 0.d0), 1.d0)
+          inc%Comp(AIR_COMP, alpha) = Ca
+          inc%Comp(WATER_COMP, alpha) = 1.d0 - Ca
+       enddo
+
+    end subroutine DiphasicFlash_enforce_consistent_molar_fractions
