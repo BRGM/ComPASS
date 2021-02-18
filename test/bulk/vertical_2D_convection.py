33a4e05229b11a8a2c088e07d29f4230a4e61de3
Simon Lopez <s.lopez@brgm.fr>
2021-02-18 23:18:52 +0100

    Update vertical 2D convection test
    Avoid the apparition of gas phase at the top of the reservoir.

--- test/bulk/vertical_2D_convection.py
+++ test/bulk/vertical_2D_convection.py
@@ -14,12 +14,9 @@ from ComPASS.linalg.factory import linear_solver
 from ComPASS.newton import Newton
 
 simulation = ComPASS.load_eos("water2ph")
-final_time = 20 * year
-# maximum output period for small time step 1.5 year
-output_period = 1.5 * year
 
 Ttop = degC2K(25.0)
-Tbot = degC2K(280.0)
+Tbot = degC2K(180.0)
 H = 3 * km
 Hcaprock = 0.1 * H
 ptop = 1 * bar
@@ -29,7 +26,7 @@ kres = 1e-12  # permeability reservoir
 phires = 0.15  # column porosity
 Kres = 2  # bulk cell thermal conductivity W/m/K
 
-nz = 101
+nz = 51
 grid = ComPASS.Grid(shape=(nz, 1, nz), extent=(H, H / nz, H), origin=(0.0, 0.0, -H),)
 
 
@@ -83,8 +80,5 @@ lsolver = linear_solver(simulation, direct=True)
 newton = Newton(simulation, 1e-5, 8, lsolver)
 
 simulation.standard_loop(
-    initial_timestep=1 * year,
-    final_time=final_time,
-    newton=newton,
-    output_period=output_period,
+    initial_timestep=year, final_time=200 * year, output_period=5 * year, newton=newton,
 )
