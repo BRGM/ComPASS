80aefcc9b2818a7d979765c998ae728c99adaae1
Simon Lopez <s.lopez@brgm.fr>
2021-06-08 17:34:47 +0200

    Fix kr for baseline tests

--- test/baseline/MO3/my_kr.py
+++ test/baseline/MO3/my_kr.py
@@ -1,71 +1,13 @@
 import numpy as np
 
-
-def fkrl(S):
-    result = np.zeros_like(S)
-    S0 = 0.3
-    dSref = 1.0 - S0
-    mask = S > S0
-    result[mask] = ((S[mask] - S0) / dSref) ** 2
-    return result
-
-
-def dfkrldS(S):
-    result = np.zeros_like(S)
-    S0 = 0.3
-    dSref = 1.0 - S0
-    mask = S > S0
-    result[mask] = (2.0 / dSref) * ((S[mask] - S0) / dSref)
-    return result
-
-
-def fkrg(S):
-    result = np.ones_like(S)
-    S0 = 0.7
-    mask = S < S0
-    result[mask] = (S[mask] / S0) ** 2
-    return result
-
-
-def dfkrgdS(s):
-    result = np.zeros_like(s)
-    S0 = 0.7
-    mask = S < S0
-    result[mask] = (2 / S0) * (S[mask] / S0)
-    return result
+# Cf. Special Panel on Geothermal Model Intercomparison Study
+# Sixth Stanford Annual Workshop on Geothermal Reservoir Engineering
+# December 16-18, 1980
+# Problem 3: 2-D Well Test
 
 
 def kr_functions(states, rocktypes, kr, dkrdS):
-    nb_phases = kr.shape[1]
-    S = states.S
-    Sg = S[:, 0]
-    Sl = S[:, 1]
-    assert nb_phases == 2, "Should not be called if np==1"
-    kr[:, 0] = fkrg(Sg)
-    dkrdS[:, 0] = dfkrgdS(Sg)
-    kr[:, 1] = fkrldS(Sl)
-    dkrdS[:, 1] = dfkrldS(Sl)
-
-
-# simulation.set_kr_functions(kr_functions)
-
-# import matplotlib.pylab as plt
-
-# S = np.linspace(0, 1)
-# plt.plot(S, fkrl(S), label="krl")
-# plt.plot(S, fkrg(1-S), label="krg")
-# plt.xlabel("Sl")
-# plt.legend()
-
-# plt.show()
-
-if __name__ == "__main__":
-    S = np.linspace(0, 1)
-    ds = 1e-7
-
-    def check(f, df):
-        dfapprox = (f(S + ds) - f(S - ds)) / (2 * ds)
-        return np.allclose(df(S), dfapprox)
-
-    print(check(fkrl, dfkrldS))
-    print(check(fkrg, dfkrgdS))
+    assert kr.shape[1] == 2, "Should not be called if np==1"
+    kr[:, 0] = 1
+    kr[:, 1] = 0
+    dkrdS[...] = 0
--- test/baseline/MO4/my_kr.py
+++ test/baseline/MO4/my_kr.py
@@ -1,71 +1,75 @@
 import numpy as np
 
+# Cf. Special Panel on Geothermal Model Intercomparison Study
+# Sixth Stanford Annual Workshop on Geothermal Reservoir Engineering
+# December 16-18, 1980
+# Problem 4: Expanding 2 Phase System with Drainage (same as problem 2)
 
-def fkrl(S):
-    result = np.zeros_like(S)
-    S0 = 0.3
-    dSref = 1.0 - S0
-    mask = S > S0
-    result[mask] = ((S[mask] - S0) / dSref) ** 2
+
+def Sstar(Sl):
+    result = np.zeros_like(Sl)
+    mask = Sl > 0.3
+    result[mask] = (Sl[mask] - 0.3) / 0.65
     return result
 
 
-def dfkrldS(S):
-    result = np.zeros_like(S)
-    S0 = 0.3
-    dSref = 1.0 - S0
-    mask = S > S0
-    result[mask] = (2.0 / dSref) * ((S[mask] - S0) / dSref)
+def dSstardSl(Sl):
+    result = np.zeros_like(Sl)
+    mask = Sl > 0.3
+    result[mask] = 1 / 0.65
     return result
 
 
-def fkrg(S):
-    result = np.ones_like(S)
-    S0 = 0.7
-    mask = S < S0
-    result[mask] = (S[mask] / S0) ** 2
-    return result
+def fkrl(Sl):
+    return Sstar(Sl) ** 4
 
 
-def dfkrgdS(s):
-    result = np.zeros_like(s)
-    S0 = 0.7
-    mask = S < S0
-    result[mask] = (2 / S0) * (S[mask] / S0)
-    return result
+def dfkrldS(Sl):
+    return 4 * dSstardSl(Sl) * (Sstar(Sl) ** 3)
+
+
+def fkrg(Sl):
+    S = Sstar(Sl)
+    return ((1 - S) ** 2) * (1 - S ** 2)
+
+
+def dfkrgdS(Sl):
+    S = Sstar(Sl)
+    dS = dSstardSl(Sl)
+    return -2 * dS * (1 - S) * ((1 - S ** 2) + (1 - S) * S)
 
 
 def kr_functions(states, rocktypes, kr, dkrdS):
-    nb_phases = kr.shape[1]
-    S = states.S
-    Sg = S[:, 0]
-    Sl = S[:, 1]
-    assert nb_phases == 2, "Should not be called if np==1"
-    kr[:, 0] = fkrg(Sg)
-    dkrdS[:, 0] = dfkrgdS(Sg)
-    kr[:, 1] = fkrldS(Sl)
-    dkrdS[:, 1] = dfkrldS(Sl)
+    assert kr.shape[1] == 2, "Should not be called if np!=2"
+    Sl = states.S[:, 1]
+    kr[:, 0] = fkrg(Sl)
+    dkrdS[:, 0, 0] = -dfkrgdS(Sl)  # derivative is w.r.t. Sg
+    kr[:, 1] = fkrl(Sl)
+    dkrdS[:, 1, 1] = dfkrldS(Sl)
 
-
-# simulation.set_kr_functions(kr_functions)
-
-# import matplotlib.pylab as plt
-
-# S = np.linspace(0, 1)
-# plt.plot(S, fkrl(S), label="krl")
-# plt.plot(S, fkrg(1-S), label="krg")
-# plt.xlabel("Sl")
-# plt.legend()
-
-# plt.show()
 
 if __name__ == "__main__":
-    S = np.linspace(0, 1)
+
+    S = np.linspace(0, 1, 100)
+
+    try:
+        import matplotlib.pyplot as plt
+    except ImportError:
+        print("No figure is drawn as matpplotlib is not installed.")
+    else:
+        plt.clf()
+        plt.plot(S, fkrl(S), label="krl")
+        plt.plot(S, fkrg(S), label="krg")
+        plt.xlabel("Sl")
+        plt.legend()
+        plt.savefig("kr.png")
+
     ds = 1e-7
 
     def check(f, df):
         dfapprox = (f(S + ds) - f(S - ds)) / (2 * ds)
         return np.allclose(df(S), dfapprox)
 
+    print(check(Sstar, dSstardSl))
     print(check(fkrl, dfkrldS))
     print(check(fkrg, dfkrgdS))
