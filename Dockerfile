1dd1cd460687f8c85cfa682325afa867ace0a52b
Simon Lopez <s.lopez@brgm.fr>
2021-06-11 21:22:34 +0000

    Use external docker image
    Docker images generation is removed from the main code repository.
    We no longer rely on dind which greatly simplifies the .gitlab-ci.yml file.

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM registry.gitlab.inria.fr/charms/compass/run-environment:latest
-
-COPY wheel/*.whl /wheel/
-RUN pip3 install --upgrade --find-links /wheel/ --no-index ComPASS \
- && rm -rf /wheel
-
-WORKDIR /localfs
--- test/fast/conftest.py
+++ test/fast/conftest.py
@@ -3,10 +3,6 @@ import os
 
 
 def pytest_configure(config):
-    # add package directory to sys.path
-    conftest_dir = os.path.abspath(os.path.dirname(__file__))
-    pkg_dir = os.path.join(conftest_dir, "../..")
-    sys.path.insert(0, pkg_dir)
 
     # configure forked option
     try:
