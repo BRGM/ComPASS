e4435a022dc6104538bc2de963b973d0948ddc7d
Simon Lopez <s.lopez@brgm.fr>
2021-06-16 18:19:20 +0200

    Modify mesh of case MO2c
    Add a more realistic version with layers and gravity.

--- test/baseline/MO2/MO2c-realistic.py
+++ test/baseline/MO2/MO2c-realistic.py
@@ -25,14 +25,14 @@ theta = np.pi / 3  # angle of the angular sector (in radians)
 rw = 0.1  # well radius
 ds = 1, 100  # target edge size around the origin and at the boundary
 H = 100  # reservoir thickness
-nb_layers = 1  # number of horizontal layers
+nb_layers = 10  # number of horizontal layers
 omega = 0.2  # reservoir porosity
 k = 1e-14  # reservoir permeability in m^2
 K = 0  # bulk thermal conductivity in W/m/K
 qw = 14.0  # total production flow rate (for the whole reservoir i.e. theta = 2pi)
 pres = 90 * bar  # reservoir pressure
 Tres = degC2K(300)
-gravity = 0
+gravity = 9.81
 
 ComPASS.set_output_directory_and_logfile(__file__)
 simulation = ComPASS.load_eos("water2ph")
@@ -40,6 +40,8 @@ simulation.set_gravity(gravity)
 simulation.set_rock_volumetric_heat_capacity(2.65e6)  # SI units J/m^3/°C
 
 mesh = extruded_sector(R, theta, ds, np.tile(H / nb_layers, nb_layers))
+# mesh_data = np.load("angular_mesh.npz")
+# mesh = MT.WedgeMesh.make(mesh_data["vertices"], mesh_data["cellnodes"])
 
 epsilon = 0.01 * ds[0]  # tolerance value to select nodes (boundary conditions...)
 
@@ -57,6 +59,7 @@ simulation.init(
     cell_porosity=omega,
     cell_permeability=k,
     cell_thermal_conductivity=K,
+    well_model="two_phases",
 )
 
 # Set the kr functions after initialization
@@ -70,8 +73,27 @@ vertices = simulation.vertices()
 x, y = vertices[:, 0], vertices[:, 1]
 simulation.reset_dirichlet_nodes((x ** 2 + y ** 2) > R ** 2 - epsilon)
 
+# tsmger = TimeStepManager(
+#     initial_timestep=1e-4 * day,
+#     maximum_timestep=20,
+#     increase_factor=1.1,
+#     decrease_factor=0.5,
+# )
+
+# lsolver = linear_solver(simulation, direct=True)
+# newton = Newton(simulation, 1e-5, 8, lsolver)
+
+# simulation.standard_loop(
+#     final_time=day, output_period=hour,
+#     newton=newton,
+#  time_step_manager=tsmger,
+#  )
+
 simulation.standard_loop(
-    initial_timestep=1e-4 * day, final_time=day, output_period=0.5 * hour,
+    initial_timestep=1e-4 * day,
+    final_time=day,
+    output_period=0.5 * hour,
+    #   time_step_manager=tsmger, well_pressure_offset=1
 )
 
 # if necessary simulation results can be directly postprocessed here
