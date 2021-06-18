fad471d794a555447e846c85a721d821a8894d16
Simon Lopez <s.lopez@brgm.fr>
2021-06-18 16:54:35 +0200

    Add new baseline test
    Problem n°1 from Molloy et al 1980.

--- test/baseline/MO1/MO1.py
+++ test/baseline/MO1/MO1.py
@@ -1,43 +1,30 @@
-import sys
-import pickle
-import yaml
-
 import numpy as np
 
 import ComPASS
 from ComPASS.utils.units import *
-
-# from ComPASS.newton import Newton
-# from ComPASS.linalg.factory import linear_solver
-from ComPASS.timeloops import TimeStepManager
-import MeshTools as MT
-
-# import ComPASS.mpi as mpi
-# from ComPASS.mpi import MPI # underlying mpi4py.MPI
-
 from ComPASS.utils.angular_sector import extruded_sector
-from my_kr import kr_functions
-
-# from generate_mesh import R, theta, ds, H, nb_layers
 
 R = 1000  # radius of the angular sector
 theta = np.pi / 3  # angle of the angular sector (in radians)
 rw = 0.1  # well radius
 ds = 1, 100  # target edge size around the origin and at the boundary
-H = 1  # reservoir thickness
+H = 100  # reservoir thickness
 nb_layers = 1  # number of horizontal layers
 omega = 0.2  # reservoir porosity
-k = 1e-14  # reservoir permeability in m^2
-K = 0  # bulk thermal conductivity in W/m/K
-qw = 14.0 / 100  # total production flow rate (for the whole reservoir i.e. theta = 2pi)
-pres = 90 * bar  # reservoir pressure
-Tres = degC2K(300)
+k = 1e-12  # reservoir permeability in m^2
+K = 20  # bulk thermal conductivity in W/m/K
+rock_heat_capacity = 1e3  # J/kg/K
+rock_density = 2500  # kg/m3
+qw = 10.0  # total injection flow rate kg/s
+pres = 50 * bar  # reservoir pressure
+Tres = degC2K(170)
+Tinj = degC2K(160)
 gravity = 0
 
 ComPASS.set_output_directory_and_logfile(__file__)
 simulation = ComPASS.load_eos("water2ph")
 simulation.set_gravity(gravity)
-simulation.set_rock_volumetric_heat_capacity(2.65e6)  # SI units J/m^3/°C
+simulation.set_rock_volumetric_heat_capacity(rock_heat_capacity * rock_density)
 
 mesh = extruded_sector(R, theta, ds, np.tile(H / nb_layers, nb_layers))
 
@@ -46,8 +33,8 @@ epsilon = 0.01 * ds[0]  # tolerance value to select nodes (boundary conditions..
 
 def make_well():
     well = simulation.create_vertical_well((0, 0), rw)
-    well.operate_on_flowrate = (theta / (2 * np.pi)) * qw, 1 * bar
-    well.produce()
+    well.operate_on_flowrate = -(theta / (2 * np.pi)) * qw, np.inf
+    well.inject(Tinj)
     return [well]
 
 
@@ -59,9 +46,6 @@ simulation.init(
     cell_thermal_conductivity=K,
 )
 
-# Set the kr functions after initialization
-simulation.set_kr_functions(kr_functions)
-
 # Set initial values
 X0 = simulation.build_state(simulation.Context.liquid, p=pres, T=Tres)
 simulation.all_states().set(X0)
@@ -70,9 +54,11 @@ vertices = simulation.vertices()
 x, y = vertices[:, 0], vertices[:, 1]
 simulation.reset_dirichlet_nodes((x ** 2 + y ** 2) > R ** 2 - epsilon)
 
+# final_time=1e9
+timestep = 1.67e7
 simulation.standard_loop(
-    initial_timestep=1e-4 * day, final_time=day, output_period=0.5 * hour,
+    fixed_timestep=timestep, final_time=60 * timestep, output_period=timestep,
 )
 
 # if necessary simulation results can be directly postprocessed here
-simulation.postprocess()
+simulation.postprocess(time_unit="second")
