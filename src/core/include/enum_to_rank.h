173fe3fcab583b7425b6c359df69117d0b88a992
Simon Lopez <s.lopez@brgm.fr>
2021-11-19 14:30:46 +0100

    Refactor enum_to_rank

--- src/core/include/enum_to_rank.h
+++ src/core/include/enum_to_rank.h
@@ -1,5 +1,17 @@
-template <typename T, typename rank_type = int>
-inline constexpr rank_type enum_to_rank(const T enum_value) {
-   assert(static_cast<rank_type>(enum_value) > 0);
-   return static_cast<rank_type>(enum_value) - 1;  // Fortran -> C indexing
+#pragma once
+
+#include <utility>
+
+// CHECKME: will be available in C++-23
+template <typename Enum>
+inline constexpr std::underlying_type_t<Enum> to_underlying(Enum e) noexcept {
+   static_assert(std::is_enum_v<Enum>);
+   return static_cast<std::underlying_type_t<Enum>>(e);
+}
+
+template <typename Enum>
+inline constexpr std::underlying_type_t<Enum> enum_to_rank(Enum e) noexcept {
+   static_assert(std::is_enum_v<Enum>);
+   assert(to_underlying(e) > 0);
+   return to_underlying(e) - 1;  // Fortran -> C indexing
 }
