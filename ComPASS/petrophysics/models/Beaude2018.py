a4a7f29a337469e546321c5454a7ebe058d1409d
Laurence Beaude <l.beaude@brgm.fr>
2021-11-23 14:46:21 +0100

    Faster Beaude2018 capillary function

--- ComPASS/petrophysics/models/Beaude2018.py
+++ ComPASS/petrophysics/models/Beaude2018.py
@@ -1,4 +1,5 @@
 import numpy as np
+import numba
 
 Pc0 = 2e5
 Sg0 = 1.0 - 1.0e-2
@@ -6,14 +7,14 @@ Sl0 = 1.0 - Sg0
 A = -Pc0 * np.log(Sl0) - (Pc0 / Sl0) * Sg0
 
 
-@np.vectorize
+@numba.vectorize(["float64(float64)"])
 def Pc(Sg):
     if Sg < Sg0:
         return -Pc0 * np.log(1.0 - Sg)
     return Pc0 * Sg / Sl0 + A
 
 
-@np.vectorize
+@numba.vectorize(["float64(float64)"])
 def dPcdS(Sg):
     if Sg < Sg0:
         return Pc0 / (1.0 - Sg)
