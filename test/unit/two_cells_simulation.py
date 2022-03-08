73208408f3be3123db964d97e960ceeb9507f40c
Simon Lopez <s.lopez@brgm.fr>
2022-03-08 10:05:36 +0100

    Add init arguments to two_cells unit test

--- test/unit/two_cells_simulation.py
+++ test/unit/two_cells_simulation.py
@@ -4,7 +4,7 @@ import ComPASS
 from ComPASS.utils.units import *
 
 
-def make():
+def make(**init_args):
     # initial reservoir pressure
     pL, pR = 2.0 * bar, 1.0 * bar
     # initial reservoir temperature - convert Celsius degrees to Kelvin degrees
@@ -28,13 +28,16 @@ def make():
         x = simulation.global_vertices()[:, 0]
         return (x == x.min()) & (x == x.max())
 
-    simulation.init(
-        mesh=grid,
-        cell_permeability=k_matrix,
-        cell_porosity=phi_matrix,
-        cell_thermal_conductivity=K_matrix,
-        set_dirichlet_nodes=dirichlet_nodes,
-    )
+    args = {
+        "mesh": grid,
+        "cell_permeability": k_matrix,
+        "cell_porosity": phi_matrix,
+        "cell_thermal_conductivity": K_matrix,
+        "set_dirichlet_nodes": dirichlet_nodes,
+    }
+    args.update(init_args)
+
+    simulation.init(**args)
 
     initial_state = simulation.build_state(simulation.Context.liquid, p=pR, T=TR)
     simulation.all_states().set(initial_state)
