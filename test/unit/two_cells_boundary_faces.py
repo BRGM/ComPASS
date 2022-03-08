061609625e124506f87ae57e4b82f3476261093b
Simon Lopez <s.lopez@brgm.fr>
2022-03-08 21:38:11 +0100

    Add unit test

---
+++ test/unit/two_cells_boundary_faces.py
@@ -0,0 +1,11 @@
+# This file is part of ComPASS.
+#
+# ComPASS is free software: you can redistribute it and/or modify it under both the terms
+# of the GNU General Public License version 3 (https://www.gnu.org/licenses/gpl.html),
+# and the CeCILL License Agreement version 2.1 (http://www.cecill.info/licences/Licence_CeCILL_V2.1-en.html).
+#
+
+
+import two_cells_simulation
+
+simulation = two_cells_simulation.make(dump_mesh_before_distribution=True)
