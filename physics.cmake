bd8d888f426df5c5823dd5b16964a696af60a7a7
Simon Lopez <s.lopez@brgm.fr>
2022-05-05 11:52:08 +0200

    Additional cmake file to choose compiled physics
    This also provides a default build configuration.
    We need to upgrade cmake version to rely on policy CMP0077.
    We add the physics.cmake configuration file to .gitignore
    not to pollute subsequent commits.

---
+++ physics.cmake
@@ -0,0 +1,15 @@
+# You can choose here which physic to build and make available in ComPASS
+# Available physics are:
+#
+# * water2ph: subcritical water with 2 phases
+# * diphasic: subcritical water with 2 phases and air component specific
+#   atmospheric boundary conditions are made available
+# * linear_water: monophasic water with super simple physical properties
+
+option(ComPASS_WITH_water2ph_PHYSICS "build water2ph module" ON)
+option(ComPASS_WITH_diphasic_PHYSICS "build diphasic module" ON)
+option(ComPASS_WITH_linear_water_PHYSICS "build linear_water module" ON)
+
+# Build all physics. If activated, it will override all previous options and
+# will include experimental physics.
+option(ComPASS_WITH_ALL_PHYSICS "build all physics modules" OFF)
