1922daa1062804d27d3ccf8dfbf861f027918ab6
Laurence Beaude <l.beaude@brgm.fr>
2022-11-04 10:17:06 +0100

    Install immiscible2ph by default

--- physics.cmake
+++ physics.cmake
@@ -2,13 +2,15 @@
 # Available physics are:
 #
 # * water2ph: subcritical water with 2 phases
-# * diphasic: subcritical water with 2 phases and air component specific
+# * diphasic: subcritical water with 2 phases and air component, specific
 #   atmospheric boundary conditions are made available
 # * linear_water: monophasic water with super simple physical properties
+# * immiscible2ph: immiscible liquid water and air in gas phase
 
 option(ComPASS_WITH_water2ph_PHYSICS "build water2ph module" ON)
 option(ComPASS_WITH_diphasic_PHYSICS "build diphasic module" ON)
 option(ComPASS_WITH_linear_water_PHYSICS "build linear_water module" ON)
+option(ComPASS_WITH_immiscible2ph_PHYSICS "build immiscible2ph module" ON)
 
 # Build all physics. If activated, it will override all previous options and
 # will include experimental physics.
