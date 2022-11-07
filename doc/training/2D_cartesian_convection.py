985e560123c8f98eda1099639716292427df8485
Laurence Beaude <l.beaude@brgm.fr>
2022-11-07 11:08:11 +0100

    Training Start of 2D cartesian convection script

---
+++ doc/training/2D_cartesian_convection.py
@@ -0,0 +1,82 @@
+# import the ComPASS library and some utilities
+# Documentation : https://charms.gitlabpages.inria.fr/ComPASS/
+import ComPASS
+from ComPASS.utils.units import *  # contains MPa, degC2K, km, year...
+
+#  |------------------------------------|
+#  |                                    |
+#  |                                    |
+#  |             Reservoir              |
+#  |                                    |
+#  |                                    |
+#  |------------------------------------|
+
+
+# Geometry
+total_depth = 240.0  # m
+
+
+# Reservoir petrophysics
+k_reservoir = 1e-12  # reservoir permeability in m^2
+omega = 0.15  # reservoir porosity
+K_reservoir = 2  # reservoir bulk thermal conductivity in W/m/K
+
+
+# -------------------------------------------------------------------
+# Set output informations
+ComPASS.set_output_directory_and_logfile(__file__)
+# Load the water2ph physics : it contains the water component
+# which can be in liquid and/or gas phase
+simulation = ComPASS.load_physics("water2ph")
+
+
+# -------------------------------------------------------------------
+# Create a Cartesian grid with cubic cells
+nx = nz = 31
+ny = 1
+d = total_depth / nz
+grid = ComPASS.Grid(
+    shape=(nx, ny, nz),
+    extent=(nx * d, ny * d, nz * d),  # creates cubic cells
+    origin=(0.0, 0.0, -total_depth),
+)
+
+
+# -------------------------------------------------------------------
+# Initialize the regionalized values and distribute the domain
+simulation.init(
+    mesh=grid,
+    cell_porosity=omega,
+    cell_thermal_conductivity=K_reservoir,
+    cell_permeability=k_reservoir,
+)
+
+
+# -------------------------------------------------------------------
+# Initialize the domain with liquid phase
+# at 10 bar and 5 Celsius degrees.
+temperature = degC2K(5.0)
+pressure = 10 * bar
+# First construct the state (at thermodynamic equilibrium)
+X0 = simulation.build_state(
+    simulation.Context.liquid,
+    p=pressure,
+    T=temperature,
+)
+# then apply this state everywhere to init the domain (constant values)
+simulation.all_states().set(X0)
+
+
+# -------------------------------------------------------------------
+# Execute the time loop
+final_time = 0.01 * year
+simulation.standard_loop(
+    initial_timestep=2 * day,
+    final_time=final_time,
+    output_period=0.5 * final_time,
+)
+
+
+# -------------------------------------------------------------------
+# Some postprocesses, it allows to visualize with Paraview
+simulation.postprocess()
