7bcbf63c68d9d669886a586ffe5ebdcb8589a204
Laurence Beaude <l.beaude@brgm.fr>
2024-01-10 15:26:48 +0100

    Extended Brooks Corey Pc

--- ComPASS/petrophysics/models/extendedBrooksCorey.py
+++ ComPASS/petrophysics/models/extendedBrooksCorey.py
@@ -1,5 +1,6 @@
 import numpy as np
 import numba
+import math
 
 # FIXME: is this a pybind11 bug?
 # This is used as global variable
@@ -9,22 +10,45 @@ import numba
 holder = None
 
 
-def BrooksCorey(Slimm, Sgimm, Pe, c2):
-    scale_g = 1.0 - Sgimm
+def no_pc():
+    @numba.vectorize(["float64(float64)"])
+    def Pc(Sg):
+        return 0.0
+
+    @numba.vectorize(["float64(float64)"])
+    def dPcdS(Sg):
+        return 0.0
+
+    return Pc, dPcdS
+
+
+def extendedBrooksCorey(Slimm, Sgimm, Pe, c2, Pcmax):
     scale_l = 1.0 - Slimm
     c2_pow = -1.0 / c2
+    erf_scale = math.sqrt(math.pi) / 2.0 / Pcmax
+    ddSerf_scale = Pcmax * 2.0 / math.sqrt(math.pi)
 
     @numba.vectorize(["float64(float64)"])
     def Pc(Sg):
-        norm_s = max((1.0 - Sg - Slimm) / scale_l, 0)
-        return Pe * norm_s**c2_pow
+        norm_s = (1.0 - Sg - Slimm) / scale_l
+        if norm_s < 0.0:
+            return Pcmax
+        Pc_tilde = Pe * norm_s**c2_pow
+        return Pcmax * math.erf(Pc_tilde * erf_scale)
 
     @numba.vectorize(["float64(float64)"])
     def dPcdS(Sg):
         norm_s = (1.0 - Sg - Slimm) / scale_l
-        if norm_s < 0:
+        if norm_s < 0.0:
             return 0.0
-        return -c2_pow * Pe / scale_l * norm_s ** (c2_pow - 1)
+        Pc_tilde = Pe * norm_s**c2_pow
+        ddSPctilde = -c2_pow * Pe / scale_l * norm_s ** (c2_pow - 1)
+        return (
+            ddSerf_scale
+            * math.exp(-((Pc_tilde * erf_scale) ** 2))
+            * ddSPctilde
+            * erf_scale
+        )
 
     return Pc, dPcdS
 
@@ -33,8 +57,14 @@ def BrooksCorey(Slimm, Sgimm, Pe, c2):
 # contains two functions : the capillary pressure Pc(Sg)
 # and the derivative dPcdS(Sg)
 laws = {
-    # rocktype=1, Slimm=0, Sgimm=0.1, Pe, c2=2
-    1: BrooksCorey(0.0, 0.1, 50, 2),
+    0: no_pc(),
+    # rocktype=1: (Slimm=0.32, Sgimm=0.1, Pe=1500, c2=2, Pcmax=9.5e4)
+    1: extendedBrooksCorey(0.32, 0.1, 1500, 2, 9.5e4),
+    2: extendedBrooksCorey(0.14, 0.1, 300, 2, 9.5e4),
+    3: extendedBrooksCorey(0.12, 0.1, 100, 2, 9.5e4),
+    4: extendedBrooksCorey(0.12, 0.1, 25, 2, 9.5e4),
+    5: extendedBrooksCorey(0.12, 0.1, 10, 2, 9.5e4),
+    6: extendedBrooksCorey(0.10, 0.1, 1, 2, 9.5e4),
 }
 
 
@@ -58,7 +88,7 @@ def phase_pressure(laws):
     return phase_pressure_function
 
 
-def set_BrooksCorey_capillary_pressure(simulation):
+def set_extendedBrooksCorey_capillary_pressure(simulation):
     # FIXME: cf. holder definition above
     global holder
 
@@ -70,13 +100,17 @@ if __name__ == "__main__":
     import matplotlib.pylab as plt
 
     n = 1000
-    Sg = np.linspace(0, 0.999, n)
-    pc = laws[1][0](Sg)
-    # pc = np.hstack([laws[1][0](Sg), laws[2][0](Sg)])
-    # Sg = np.hstack([Sg, Sg])
-    plt.plot(Sg, pc, ".")
-    # plt.ylim(1e-1, 1e15)
+    Sg = np.linspace(0, 1, n)
+    pc1 = laws[1][0](Sg)
+    pc3 = laws[3][0](Sg)
+    pc4 = laws[4][0](Sg)
+    pc5 = laws[5][0](Sg)
+    plt.plot(1.0 - Sg, pc1, ".")
+    plt.plot(1.0 - Sg, pc3, ".")
+    plt.plot(1.0 - Sg, pc4, ".")
+    plt.plot(1.0 - Sg, pc5, ".")
+    # plt.ylim(1e3, 1.2e5)
     plt.yscale("log")
-    plt.xlabel("Sg")
-    plt.ylabel("Brooks Corey Pc (rt = 1)")
+    plt.xlabel("Sl")
+    plt.ylabel("extended Brooks Corey Pc (rt = 1, 3, 4, 5)")
     plt.savefig("Pc.png")
--- ComPASS/simulation/fake_methods.py
+++ ComPASS/simulation/fake_methods.py
@@ -60,7 +60,9 @@ from ..petrophysics.kr import set_kr_functions
 from ..petrophysics.phase_pressure import set_phase_pressure_functions
 from ..petrophysics.capillary import set_liquid_capillary_pressure
 from ..petrophysics.models.vanGenuchten import set_vanGenuchten_capillary_pressure
-from ..petrophysics.models.BrooksCorey import set_BrooksCorey_capillary_pressure
+from ..petrophysics.models.extendedBrooksCorey import (
+    set_extendedBrooksCorey_capillary_pressure,
+)
 from ..properties.utils import Xalpha, empty_Xalpha
 from ..properties.physical_properties_wrappers import (
     liquid_dynamic_viscosity,
