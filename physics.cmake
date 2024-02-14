d48f7b9352637b8e13e4b41de6424a41ad45a5c3
Laurence Beaude <l.beaude@brgm.fr>
2024-02-14 11:23:51 +0100

    Add diphasicCO2 in physics.cmake
    To test diphasicCO2 in CI

--- physics.cmake
+++ physics.cmake
@@ -4,11 +4,13 @@
 # * water2ph: subcritical water with 2 phases
 # * diphasic: subcritical water with 2 phases and air component, specific
 #   atmospheric boundary conditions are made available
+# * diphasicCO2: subcritical water with 2 phases and CO2 component
 # * linear_water: monophasic water with super simple physical properties
 # * immiscible2ph: immiscible liquid water and air in gas phase
 
 option(ComPASS_WITH_water2ph_PHYSICS "build water2ph module" ON)
 option(ComPASS_WITH_diphasic_PHYSICS "build diphasic module" ON)
+option(ComPASS_WITH_diphasicCO2_PHYSICS "build diphasicCO2 module" ON)
 option(ComPASS_WITH_linear_water_PHYSICS "build linear_water module" ON)
 option(ComPASS_WITH_immiscible2ph_PHYSICS "build immiscible2ph module" ON)
 
