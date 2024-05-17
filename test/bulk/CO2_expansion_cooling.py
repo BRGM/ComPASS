bc084af5f54bcea1c19b2b2fb8e18a5c751dfb81
Laurence Beaude <l.beaude@brgm.fr>
2024-05-17 17:39:17 +0200

    Deactivate CO2 tests not compatible with CO2 model
    The thermo is an approximation for 310K < T < 350K and 20Mpa < P <
    40Mpa, which is in the supercritique zone, so there is no gas only possibility

--- test/bulk/CO2_expansion_cooling.py
+++ test/bulk/CO2_expansion_cooling.py
@@ -14,74 +14,80 @@ from ComPASS.timeloops import standard_loop
 import ComPASS.messages
 from ComPASS.timestep_management import TimeStepManager
 
-
-Lz = 4000.0
-nz = 400
-dz = Lz / nz
-Lx = Ly = dz
-Ox, Oy, Oz = 0.0, 0.0, -3000.0
-nx = ny = 1
-Topz = Oz + Lz
-
-omega_reservoir = 0.35  # reservoir porosity
-k_reservoir = 1e-12  # reservoir permeability in m^2, 1D = 10^-12 m^
-cell_thermal_cond = 3.0  # reservoir thermal conductivity
-pall = 1.0e5  # init Pressure
-pbot_dir = 1.2e5  # bottom Pressure
-Tall = 700.0  # top Temperature
-Tbot_dir = 750.0  # bottom Temperature
-gravity = 9.81
-
-simulation = ComPASS.load_physics("diphasicCO2")
-simulation.set_gravity(gravity)
-ComPASS.set_output_directory_and_logfile(__file__)
-
-
-grid = ComPASS.Grid(
-    shape=(nx, ny, nz),
-    extent=(Lx, Ly, Lz),
-    origin=(Ox, Oy, Oz),
+print(
+    "**************************  WARNING  **************************\n"
+    "this test is not possible anymore with the diphasicCO2 eos because the thermo\n",
+    "is an approximation for 310K < T < 350K and 20Mpa < P < 40Mpa\n",
+    "which is in the supercritique zone, so there is no gas only possibility.",
 )
 
+# Lz = 4000.0
+# nz = 400
+# dz = Lz / nz
+# Lx = Ly = dz
+# Ox, Oy, Oz = 0.0, 0.0, -3000.0
+# nx = ny = 1
+# Topz = Oz + Lz
 
-def Dirichlet_node():
-    vertices = np.rec.array(simulation.global_vertices())
-    return vertices[:, 2] <= Oz
+# omega_reservoir = 0.35  # reservoir porosity
+# k_reservoir = 1e-12  # reservoir permeability in m^2, 1D = 10^-12 m^
+# cell_thermal_cond = 3.0  # reservoir thermal conductivity
+# pall = 1.0e5  # init Pressure
+# pbot_dir = 1.2e5  # bottom Pressure
+# Tall = 700.0  # top Temperature
+# Tbot_dir = 750.0  # bottom Temperature
+# gravity = 9.81
 
-
-simulation.init(
-    mesh=grid,
-    set_dirichlet_nodes=Dirichlet_node,
-    cell_porosity=omega_reservoir,
-    cell_permeability=k_reservoir,
-    cell_thermal_conductivity=cell_thermal_cond,
-)
-
-X0 = simulation.build_state(simulation.Context.gas, p=pall, T=Tall)
-simulation.all_states().set(X0)
-XDir = simulation.build_state(simulation.Context.gas, p=pbot_dir, T=Tbot_dir)
-simulation.dirichlet_node_states().set(XDir)
-
-# example on how to get the liquid molar enthalpy with Cal=0.8, Cwl=0.2
-# mol_enth_ex = simulation.cpp_liquid_molar_enthalpy(pall, Tall, [0.8, 0.2])
-
-timestep = TimeStepManager(
-    initial_timestep=100.0,
-    minimum_timestep=1e-1,
-    maximum_timestep=50.0 * year,
-    increase_factor=1.2,
-    decrease_factor=0.2,
-)
-
-final_time = 1000.0 * year
-output_period = 0.01 * final_time
+# simulation = ComPASS.load_physics("diphasicCO2")
+# simulation.set_gravity(gravity)
+# ComPASS.set_output_directory_and_logfile(__file__)
 
 
-current_time = standard_loop(
-    simulation,
-    final_time=final_time,
-    time_step_manager=timestep,
-    output_period=output_period,
-)
+# grid = ComPASS.Grid(
+#     shape=(nx, ny, nz),
+#     extent=(Lx, Ly, Lz),
+#     origin=(Ox, Oy, Oz),
+# )
 
-simulation.postprocess(convert_temperature=True, time_unit="year")
+
+# def Dirichlet_node():
+#     vertices = np.rec.array(simulation.global_vertices())
+#     return vertices[:, 2] <= Oz
+
+
+# simulation.init(
+#     mesh=grid,
+#     set_dirichlet_nodes=Dirichlet_node,
+#     cell_porosity=omega_reservoir,
+#     cell_permeability=k_reservoir,
+#     cell_thermal_conductivity=cell_thermal_cond,
+# )
+
+# X0 = simulation.build_state(simulation.Context.gas, p=pall, T=Tall)
+# simulation.all_states().set(X0)
+# XDir = simulation.build_state(simulation.Context.gas, p=pbot_dir, T=Tbot_dir)
+# simulation.dirichlet_node_states().set(XDir)
+
+# # example on how to get the liquid molar enthalpy with Cal=0.8, Cwl=0.2
+# # mol_enth_ex = simulation.cpp_liquid_molar_enthalpy(pall, Tall, [0.8, 0.2])
+
+# timestep = TimeStepManager(
+#     initial_timestep=100.0,
+#     minimum_timestep=1e-1,
+#     maximum_timestep=50.0 * year,
+#     increase_factor=1.2,
+#     decrease_factor=0.2,
+# )
+
+# final_time = 1000.0 * year
+# output_period = 0.01 * final_time
+
+
+# current_time = standard_loop(
+#     simulation,
+#     final_time=final_time,
+#     time_step_manager=timestep,
+#     output_period=output_period,
+# )
+
+# simulation.postprocess(convert_temperature=True, time_unit="year")
--- test/bulk/CO2_gas_to_liquid.py
+++ test/bulk/CO2_gas_to_liquid.py
@@ -13,71 +13,78 @@ from ComPASS.timeloops import standard_loop
 import ComPASS.messages
 from ComPASS.timestep_management import TimeStepManager
 
-Lz = 1000.0
-nz = 200
-dz = Lz / nz
-Lx = Ly = 2 * dz
-Ox, Oy, Oz = 0.0, 0.0, -800.0
-nx = ny = 1
-
-omega_reservoir = 0.35  # reservoir porosity
-k_reservoir = 1e-12  # reservoir permeability in m^2, 1D = 10^-12 m^
-cell_thermal_cond = 3.0  # reservoir thermal conductivity
-pall = 1.0e7  # top Pressure
-pbot_dir = 2.0e7  # bottom Pressure
-Tall = degC2K(5.0)  # top Temperature
-Tbot_dir = degC2K(150.0)  # bottom Temperature
-gravity = 9.81
-
-simulation = ComPASS.load_physics("diphasicCO2")
-simulation.set_gravity(gravity)
-ComPASS.set_output_directory_and_logfile(__file__)
-
-
-grid = ComPASS.Grid(
-    shape=(nx, ny, nz),
-    extent=(Lx, Ly, Lz),
-    origin=(Ox, Oy, Oz),
+print(
+    "**************************  WARNING  **************************\n"
+    "this test is not possible anymore with the diphasicCO2 eos because the thermo\n",
+    "is an approximation for 310K < T < 350K and 20Mpa < P < 40Mpa\n",
+    "which is in the supercritique zone, so there is no gas only possibility.",
 )
 
+# Lz = 1000.0
+# nz = 200
+# dz = Lz / nz
+# Lx = Ly = 2 * dz
+# Ox, Oy, Oz = 0.0, 0.0, -800.0
+# nx = ny = 1
 
-def Dirichlet_node():
-    vertices = np.rec.array(simulation.global_vertices())
-    return vertices[:, 2] <= Oz
+# omega_reservoir = 0.35  # reservoir porosity
+# k_reservoir = 1e-12  # reservoir permeability in m^2, 1D = 10^-12 m^
+# cell_thermal_cond = 3.0  # reservoir thermal conductivity
+# pall = 1.0e7  # top Pressure
+# pbot_dir = 2.0e7  # bottom Pressure
+# Tall = degC2K(5.0)  # top Temperature
+# Tbot_dir = degC2K(150.0)  # bottom Temperature
+# gravity = 9.81
+
+# simulation = ComPASS.load_physics("diphasicCO2")
+# simulation.set_gravity(gravity)
+# ComPASS.set_output_directory_and_logfile(__file__)
 
 
-simulation.init(
-    mesh=grid,
-    set_dirichlet_nodes=Dirichlet_node,
-    cell_porosity=omega_reservoir,
-    cell_permeability=k_reservoir,
-    cell_thermal_conductivity=cell_thermal_cond,
-)
+# grid = ComPASS.Grid(
+#     shape=(nx, ny, nz),
+#     extent=(Lx, Ly, Lz),
+#     origin=(Ox, Oy, Oz),
+# )
 
-# start with gas to test context changes (from gas to diphasic, from diphasic t liquid)
-X0 = simulation.build_state(simulation.Context.gas, p=1.0e5, T=500)
-simulation.all_states().set(X0)
-XDir = simulation.build_state(simulation.Context.liquid, p=pbot_dir, T=Tbot_dir)
-simulation.dirichlet_node_states().set(XDir)
 
-# example on how to get the liquid molar enthalpy with Cal=0.8, Cwl=0.2
-# mol_enth_ex = simulation.cpp_liquid_molar_enthalpy(pall, Tall, [0.8, 0.2])
+# def Dirichlet_node():
+#     vertices = np.rec.array(simulation.global_vertices())
+#     return vertices[:, 2] <= Oz
 
-timestep = TimeStepManager(
-    initial_timestep=30,
-    minimum_timestep=1e-1,
-    increase_factor=1.3,
-    decrease_factor=0.2,
-)
 
-final_time = 1.0 * year
-output_period = 0.1 * final_time
+# simulation.init(
+#     mesh=grid,
+#     set_dirichlet_nodes=Dirichlet_node,
+#     cell_porosity=omega_reservoir,
+#     cell_permeability=k_reservoir,
+#     cell_thermal_conductivity=cell_thermal_cond,
+# )
 
-current_time = standard_loop(
-    simulation,
-    final_time=final_time,
-    time_step_manager=timestep,
-    output_period=output_period,
-)
+# # start with gas to test context changes (from gas to diphasic, from diphasic t liquid)
+# X0 = simulation.build_state(simulation.Context.gas, p=1.0e5, T=500)
+# simulation.all_states().set(X0)
+# XDir = simulation.build_state(simulation.Context.liquid, p=pbot_dir, T=Tbot_dir)
+# simulation.dirichlet_node_states().set(XDir)
 
-simulation.postprocess(convert_temperature=True, time_unit="year")
+# # example on how to get the liquid molar enthalpy with Cal=0.8, Cwl=0.2
+# # mol_enth_ex = simulation.cpp_liquid_molar_enthalpy(pall, Tall, [0.8, 0.2])
+
+# timestep = TimeStepManager(
+#     initial_timestep=30,
+#     minimum_timestep=1e-1,
+#     increase_factor=1.3,
+#     decrease_factor=0.2,
+# )
+
+# final_time = 1.0 * year
+# output_period = 0.1 * final_time
+
+# current_time = standard_loop(
+#     simulation,
+#     final_time=final_time,
+#     time_step_manager=timestep,
+#     output_period=output_period,
+# )
+
+# simulation.postprocess(convert_temperature=True, time_unit="year")
