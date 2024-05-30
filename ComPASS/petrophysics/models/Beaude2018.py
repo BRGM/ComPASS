40ae0722546dde5c69df1a033d626f753c9f1857
Laurence Beaude <l.beaude@brgm.fr>
2024-05-30 17:53:58 +0200

    Plot Beaude2018 pc

--- ComPASS/petrophysics/models/Beaude2018.py
+++ ComPASS/petrophysics/models/Beaude2018.py
@@ -19,3 +19,19 @@ def dPcdS(Sg):
     if Sg < Sg0:
         return Pc0 / (1.0 - Sg)
     return Pc0 / Sl0
+
+
+if __name__ == "__main__":
+    import matplotlib.pylab as plt
+
+    n = 1000
+    Sg = np.linspace(0, 1, n)
+    pc = Pc(Sg)
+    # pc = np.hstack([laws[1][0](Sg), laws[2][0](Sg)])
+    # Sg = np.hstack([Sg, Sg])
+    plt.plot(1.0 - Sg, pc, ".")
+    # plt.ylim(1e3, 1.2e5)
+    plt.yscale("log")
+    plt.xlabel("Sl")
+    plt.ylabel("Beaude2018")
+    plt.savefig("Pc_Beaude.png")
