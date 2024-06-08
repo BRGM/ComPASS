6c46b25f3bd95698533723ee991c9bf18c88081e
Laurence Beaude <l.beaude@brgm.fr>
2024-06-08 07:59:58 +0200

    Remove old fortran max Newton increment

--- src/core/SchemeParameters.F90
+++ src/core/SchemeParameters.F90
@@ -12,13 +12,14 @@ module SchemeParameters
 
    implicit none
 
-   ! ! ****** Obj values used to compute Newton increment ****** ! !
+   ! defined in python
+   ! ! ! ****** Obj values used to compute Newton increment ****** ! !
 
-   real(c_double), parameter :: &
-      NewtonIncreObj_P = 5.001d6, &
-      NewtonIncreObj_T = 20.d0, &
-      NewtonIncreObj_C = 1.d0, &
-      NewtonIncreObj_S = 0.2d0
+   ! real(c_double), parameter :: &
+   !    NewtonIncreObj_P = 5.001d6, &
+   !    NewtonIncreObj_T = 20.d0, &
+   !    NewtonIncreObj_C = 1.d0, &
+   !    NewtonIncreObj_S = 0.2d0
 
    ! eps
    real(c_double), parameter :: eps = 1.0d-10
