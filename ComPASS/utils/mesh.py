6c767d41ff8164a81b4b8631142622c9d947622b
Simon Lopez <s.lopez@brgm.fr>
2024-06-19 11:42:46 +0200

    More extrusion utilities

--- ComPASS/utils/mesh.py
+++ ComPASS/utils/mesh.py
@@ -65,6 +65,80 @@ def extrude(points, cells, thickness=1.0, axis=2):
     )
 
 
+def extrude_triangle_strips(Lx, nx, dz, nbstrips=1):
+    """
+    :param Lx: length of the strips along Ox
+    :param nx: number of points along Ox
+    :param dz: height of the strips
+    :param nbstrips: number of strips
+    """
+    assert nbstrips > 0
+    nz = nbstrips + 1
+    assert nx >= 2 and nz >= 2  # 2 points for at least one cell
+    x = np.linspace(0, Lx, nx)
+    dx = x[1] - x[0]
+    assert dx > 0
+    # x staggered
+    xs = np.hstack([x[0], x[1:] - 0.5 * dx, x[-1]])
+    pts = []
+    for k in range(nz):
+        if k % 2 == 0:
+            pts.append(np.vstack([x, np.tile(k * dz, nx)]).T)
+        else:
+            pts.append(np.vstack([xs, np.tile(k * dz, nx + 1)]).T)
+    pts = np.vstack(pts)
+    triangles = []
+    for k in range(nz - 1):
+        O = k * nx + k // 2
+        if k % 2 == 0:
+            triangles.append(
+                np.vstack(
+                    [
+                        np.array([[O, O + nx, O + nx + 1], [O, O + nx + 1, O + 1]]) + i
+                        for i in range(nx - 1)
+                    ]
+                    + [[O + nx - 1, O + 2 * nx - 1, O + 2 * nx]]
+                )
+            )
+        else:
+            triangles.append(
+                np.vstack(
+                    [
+                        np.array(
+                            [[O, O + nx + 1, O + 1], [O + nx + 1, O + nx + 2, O + 1]]
+                        )
+                        + i
+                        for i in range(nx - 1)
+                    ]
+                    + [[O + nx - 1, O + 2 * nx, O + nx]]
+                )
+            )
+    triangles = np.vstack(triangles)
+    return extrude(pts, triangles, axis=1, thickness=dz)
+
+
+def extrude_quad_strips(Lx, nx, dz, nbstrips=1):
+    """
+    :param Lx: length of the strips along Ox
+    :param nx: number of points along Ox
+    :param dz: height of the strips
+    :param nbstrips: number of strips
+    """
+    assert nbstrips > 0
+    nz = nbstrips + 1
+    assert nx >= 2 and nz >= 2  # 2 points for at least one cell
+    x = np.linspace(0, Lx, nx)
+    dx = x[1] - x[0]
+    assert dx > 0
+    pts = []
+    for k in range(nz):
+        pts.append(np.vstack([x, np.tile(k * dz, nx)]).T)
+    pts = np.vstack(pts)
+    row = np.vstack([np.array([0, 1, nx + 1, nx]) + k for k in range(nx - 1)])
+    quads = np.vstack([row + k * nx for k in range(nz - 1)])
+    return extrude(pts, quads, axis=1, thickness=dz)
+
+
 if __name__ == "__main__":
     import vtkwriters as vtkw
 
